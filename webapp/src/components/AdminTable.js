a388c9af2ed505b6d566edffe1147e59c52c684d
jacob6838 <jfrye@neaeraconsulting.com>
2023-04-27 17:16:21 -0600

    abstracting AdminTable theme

--- webapp/src/components/AdminTable.js
+++ webapp/src/components/AdminTable.js
@@ -1,57 +1,14 @@
 import React from 'react'
 import MaterialTable from '@material-table/core'
-import { ThemeProvider, createTheme } from '@mui/material'
+import { ThemeProvider } from '@mui/material'
+import { theme } from '../styles'
 
 import '../features/adminRsuTab/Admin.css'
 
 const AdminTable = (props) => {
-  const defaultMaterialTheme = createTheme({
-    palette: {
-      common: {
-        black: '#000000',
-        white: '#ffffff',
-      },
-      primary: {
-        main: '#d16d15',
-        light: '#0e2052',
-        contrastTextColor: '#0e2052',
-      },
-      secondary: {
-        main: '#d16d15',
-        light: '#0e2052',
-        contrastTextColor: '#0e2052',
-      },
-      text: {
-        primary: '#ffffff',
-        secondary: '#ffffff',
-        disabled: '#ffffff',
-        hint: '#ffffff',
-      },
-      divider: '#333',
-      background: {
-        paper: '#333',
-        default: '#1c1d1f',
-      },
-    },
-    components: {
-      MuiIcon: {
-        styleOverrides: {
-          root: {
-            // Match 24px = 3 * 2 + 1.125 * 16
-            color: '#d16d15',
-          },
-        },
-      },
-      TextField: {},
-    },
-    input: {
-      color: '#11ff00',
-    },
-  })
-
   return (
     <div>
-      <ThemeProvider theme={defaultMaterialTheme}>
+      <ThemeProvider theme={theme}>
         <MaterialTable
           actions={props.actions}
           columns={props.columns}

d823d25db3fab7fecd83971c260cb9af04167b05
Michael7371 <michael@neaera.net>
2023-05-04 13:10:54 -0600

    unit tests

--- api/tests/data/rsu_geo_query_data.py
+++ api/tests/data/rsu_geo_query_data.py
@@ -2,55 +2,47 @@ import multidict
 
 ##################################### request_data ###########################################
 
-request_args_good = multidict.MultiDict([
-        ('geometry', [[-105.18530732421814,39.727751014682724],[-105.42357331542925,39.75572678980467],[-105.4228866699211,39.65644066619139],[-105.12213593749924,39.680218966965384],[-105.18530732421814,39.727751014682724]])
-        ])
+request_args_good = multidict.MultiDict(
+    [
+        (
+            "geometry",
+            [
+                [-105.18530732421814, 39.727751014682724],
+                [-105.42357331542925, 39.75572678980467],
+                [-105.4228866699211, 39.65644066619139],
+                [-105.12213593749924, 39.680218966965384],
+                [-105.18530732421814, 39.727751014682724],
+            ],
+        )
+    ]
+)
 
-request_args_bad_message = multidict.MultiDict([
-        ('geometry', [[5.1], 10.444])
-        ])
+request_args_bad_message = multidict.MultiDict([("geometry", [[5.1], 10.444])])
 
-request_args_bad_type = multidict.MultiDict([
-        ('geometry', 'bad type')
-        ])
+request_args_bad_type = multidict.MultiDict([("geometry", "bad type")])
 
-request_params_good = multidict.MultiDict([
-        ('user_info', {'organizations': [{'name': 'Test', 'role': 'user'}]}),
-        ('organization', 'Test')
-        ])
+request_params_good = multidict.MultiDict(
+    [
+        ("user_info", {"organizations": [{"name": "Test", "role": "user"}]}),
+        ("organization", "Test"),
+    ]
+)
 
-##################################### query_rsu_counts ###########################################
+##################################### query_org_rsus ###########################################
 
-rsu_counts_query = "SELECT RSU, Road, SUM(Count) as Count FROM `Fake_table` " \
-                "WHERE Date >= DATETIME(\"2022-05-23T18:00:00\") " \
-                "AND Date < DATETIME(\"2022-05-24T18:00:00\") " \
-                "AND Type = \"BSM\" GROUP BY RSU, Road "
+rsu_org_query = (
+    "SELECT ipv4_address from public.rsus as rd "
+    "JOIN public.rsu_organization_name AS ron_v ON ron_v.rsu_id = rd.rsu_id "
+    f"WHERE ron_v.name = 'Test'"
+)
 
-rsu_one = multidict.MultiDict([
-        ('RSU', '10.11.81.24'), 
-        ('Road', 'Region 1'),
-        ('Count', '0')
-    ])
-rsu_two = multidict.MultiDict([
-    ('RSU', '172.16.28.136'), 
-    ('Road', 'I70'),
-    ('Count', '8472')
-])
-rsu_three = multidict.MultiDict([
-    ('RSU', '172.16.28.23'), 
-    ('Road', 'C-470'),
-    ('Count', '0')
-])
+point_list = [
+    [-105.34460908203145, 39.724583197251334],
+    [-105.34666901855489, 39.670180083300174],
+    [-105.25122529296911, 39.679162192647944],
+    [-105.2539718750002, 39.72088725644132],
+    [-105.34460908203145, 39.724583197251334],
+]
 
-rsu_counts_expected_single = {"10.11.81.24": {"road": "Region 1", "count": "0"}}
 
-rsu_counts_expected_multiple = {
-        "10.11.81.24": {"road": "Region 1", "count": "0"},
-        "172.16.28.136": {"road": "I70", "count": "8472"},
-        "172.16.28.23": {"road": "C-470", "count": "0"}
-        }
-
-rsu_counts_expected_limited_rsus = {
-        "172.16.28.136": {"road": "I70", "count": "8472"},
-        "172.16.28.23": {"road": "C-470", "count": "0"}
-        }
+rsu_devices_query = "SELECT ipv4_address as Ip, ST_X(geography::geometry) AS long, ST_Y(geography::geometry) AS lat FROM rsus WHERE ipv4_address = ANY('{10.11.81.12}'::inet[]) AND ST_Contains(ST_SetSRID(ST_GeomFromText('POLYGON((-105.34460908203145 39.724583197251334,-105.34666901855489 39.670180083300174,-105.25122529296911 39.679162192647944,-105.2539718750002 39.72088725644132,-105.34460908203145 39.724583197251334))'), 4326), rsus.geography::geometry)"

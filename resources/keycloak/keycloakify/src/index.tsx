d71b6613b35dc12a3127efb4ba8d04801bbe8d55
Michael7371 <40476797+Michael7371@users.noreply.github.com>
2023-09-12 11:31:40 -0600

    removed unnecessary user configuration from keycloakify

--- resources/keycloak/keycloakify/src/index.tsx
+++ resources/keycloak/keycloakify/src/index.tsx
@@ -1,30 +1,19 @@
-import { createRoot } from "react-dom/client";
-import { StrictMode, lazy, Suspense } from "react";
-import { kcContext as kcLoginThemeContext } from "./login/kcContext";
-import { kcContext as kcAccountThemeContext } from "./account/kcContext";
+import { createRoot } from 'react-dom/client'
+import { StrictMode, lazy, Suspense } from 'react'
+import { kcContext as kcLoginThemeContext } from './login/kcContext'
 
-const KcLoginThemeApp = lazy(() => import("./login/KcApp"));
-const KcAccountThemeApp = lazy(() => import("./account/KcApp"));
+const KcLoginThemeApp = lazy(() => import('./login/KcApp'))
 
-createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <Suspense>
-            {(()=>{
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Suspense>
+      {(() => {
+        if (kcLoginThemeContext !== undefined) {
+          return <KcLoginThemeApp kcContext={kcLoginThemeContext} />
+        }
 
-                if( kcLoginThemeContext !== undefined ){
-                    return <KcLoginThemeApp kcContext={kcLoginThemeContext} />;
-                }
-
-                if( kcAccountThemeContext !== undefined ){
-                    return <KcAccountThemeApp kcContext={kcAccountThemeContext} />;
-                }
-
-                throw new Error(
-                  "This app is a Keycloak theme" +
-                  "It isn't meant to be deployed outside of Keycloak"
-                );
-
-            })()}
-        </Suspense>
-    </StrictMode>
-);
+        throw new Error('This app is a Keycloak theme' + "It isn't meant to be deployed outside of Keycloak")
+      })()}
+    </Suspense>
+  </StrictMode>
+)

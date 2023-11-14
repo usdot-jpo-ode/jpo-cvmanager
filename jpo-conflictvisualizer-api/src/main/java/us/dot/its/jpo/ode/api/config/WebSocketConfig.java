a11170a7ceb363d88259316dff8ba621f2827331
John-Wiens <jwiens3141@gmail.com>
2023-11-14 11:31:26 -0700

    Added Basic Websocket Proof-of-Concept Code

---
+++ jpo-conflictvisualizer-api/src/main/java/us/dot/its/jpo/ode/api/config/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package us.dot.its.jpo.ode.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import us.dot.its.jpo.ode.api.controllers.LiveFeedController;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        System.out.println("Adding Live to Websocket Registry");
+		registry.addHandler(new LiveFeedController(), "/live").setAllowedOrigins("*");
+	}
+
+}
---
+++ jpo-conflictvisualizer-api/src/main/java/us/dot/its/jpo/ode/api/controllers/LiveFeedController.java
@@ -0,0 +1,23 @@
+package us.dot.its.jpo.ode.api.controllers;
+
+
+import java.io.IOException;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class LiveFeedController extends TextWebSocketHandler {
+
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message)
+			throws InterruptedException, IOException {
+
+		String payload = message.getPayload();
+		JSONObject jsonObject = new JSONObject(payload);
+		session.sendMessage(new TextMessage("Hi " + jsonObject.get("user") + " how may we help you?"));
+	}
+}

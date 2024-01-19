04f4931d3fbc9336c8de206466fcfb077f613915
Drew Johnston <31270488+drewjj@users.noreply.github.com>
2024-01-18 22:39:38 -0700

    Fully tested in k8s with these changes

--- services/addons/images/firmware_manager/yunex_upgrader.py
+++ services/addons/images/firmware_manager/yunex_upgrader.py
@@ -12,7 +12,9 @@ class YunexUpgrader( upgrader.UpgraderAbstractClass ):
     super().__init__(upgrade_info)
 
   def run_xfer_upgrade(self, file_name):
-    proc = subprocess.run(["java", "-jar", f"/home/tools/xfer_yunex.jar", "-upload", file_name, f"{self.rsu_ip}:3600"], capture_output=True)
+    xfer_command = ["java", "-jar", f"/home/tools/xfer_yunex.jar", "-upload", file_name, f"{self.rsu_ip}:3600"]
+    logging.info(xfer_command)
+    proc = subprocess.run(xfer_command, capture_output=True)
     code, stdout, stderr = proc.returncode, proc.stdout, proc.stderr
 
     # If the command ends with a non-successful status code, return -1
@@ -32,11 +34,14 @@ class YunexUpgrader( upgrader.UpgraderAbstractClass ):
   def wait_until_online(self):
     logging.info("Pinging RSU until online")
     iter = 0
-    while iter < 100:
-      code = subprocess.run(['ping', '-n', '-w5', '-c3', self.rsu_ip], capture_output=True).returncode
+    # Ping once every second for 3 minutes until online
+    while iter < 180:
+      code = subprocess.run(['ping', '-n', '-c1', self.rsu_ip], capture_output=True).returncode
       if code == 0:
         return 0
+      time.sleep(1)
       iter += 1
+    # 3 minutes pass with no response
     return -1
 
   def upgrade(self):

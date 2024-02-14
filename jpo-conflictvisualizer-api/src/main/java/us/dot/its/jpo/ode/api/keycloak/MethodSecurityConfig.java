e67391035ef4c88ddd22ae517c531e082b1e2d92
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2024-02-14 16:54:47 -0700

    JWT Converter code to extract user name and roles

---
+++ jpo-conflictvisualizer-api/src/main/java/us/dot/its/jpo/ode/api/keycloak/MethodSecurityConfig.java
@@ -0,0 +1,45 @@
+package us.dot.its.jpo.ode.api.keycloak;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+
+
+/**
+ * Enables security annotations via like {@link org.springframework.security.access.prepost.PreAuthorize} and
+ * {@link org.springframework.security.access.prepost.PostAuthorize} annotations per-method.
+ */
+@Configuration
+@RequiredArgsConstructor
+@EnableMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
+class MethodSecurityConfig {
+
+    private final ApplicationContext applicationContext;
+
+    private final PermissionEvaluator permissionEvaluator;
+
+    @Bean
+    MethodSecurityExpressionHandler customMethodSecurityExpressionHandler() {
+
+        var expressionHandler = new DefaultMethodSecurityExpressionHandler();
+        expressionHandler.setApplicationContext(applicationContext);
+        expressionHandler.setPermissionEvaluator(permissionEvaluator);
+        return expressionHandler;
+    }
+
+    @Bean
+    GrantedAuthoritiesMapper keycloakAuthoritiesMapper() {
+
+        var mapper = new SimpleAuthorityMapper();
+        mapper.setConvertToUpperCase(true);
+        return mapper;
+    }
+
+}

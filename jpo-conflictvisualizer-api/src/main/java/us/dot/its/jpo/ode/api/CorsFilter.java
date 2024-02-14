78089dcc4457cd89889a0a3a55eae0a180e9c064
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2024-02-14 14:22:21 -0700

    Do CORS more like the right way

--- jpo-conflictvisualizer-api/src/main/java/us/dot/its/jpo/ode/api/CorsFilter.java
+++ jpo-conflictvisualizer-api/src/main/java/us/dot/its/jpo/ode/api/CorsFilter.java
@@ -1,33 +1,33 @@
-package us.dot.its.jpo.ode.api;
-
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-
-/**
- * Custom servlet filter to add CORS header
- */
-public class CorsFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        // Nothing to initialize
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        var response = (HttpServletResponse)servletResponse;
-        var request = (HttpServletRequest)servletRequest;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "OPTIONS,GET,POST,DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "authorization");
-        response.setIntHeader("Access-Control-Max-Age", 1800);
-        filterChain.doFilter(servletRequest, servletResponse);
-    }
-
-    @Override
-    public void destroy() {
-        // Nothing to destroy
-    }
-}
+//package us.dot.its.jpo.ode.api;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//import java.io.IOException;
+//
+///**
+// * Custom servlet filter to add CORS header
+// */
+//public class CorsFilter implements Filter {
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        // Nothing to initialize
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        var response = (HttpServletResponse)servletResponse;
+//        var request = (HttpServletRequest)servletRequest;
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "OPTIONS,GET,POST,DELETE");
+//        response.setHeader("Access-Control-Allow-Headers", "authorization");
+//        response.setIntHeader("Access-Control-Max-Age", 1800);
+//        filterChain.doFilter(servletRequest, servletResponse);
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        // Nothing to destroy
+//    }
+//}

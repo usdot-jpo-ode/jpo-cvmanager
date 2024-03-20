5b0c1239ef294e2d1adf29f3b99d12f9fa38f7b6
Marc Wodahl <mwodahl@trihydro.com>
2024-03-20 14:29:50 -0600

    Add post_upgrade method and functionality

--- services/addons/images/firmware_manager/commsignia_upgrader.py
+++ services/addons/images/firmware_manager/commsignia_upgrader.py
@@ -1,3 +1,5 @@
+import subprocess
+import time
 from paramiko import SSHClient, WarningPolicy
 from scp import SCPClient
 import upgrader
@@ -9,6 +11,9 @@ import sys
 
 class CommsigniaUpgrader(upgrader.UpgraderAbstractClass):
     def __init__(self, upgrade_info):
+        # set file/blob location for post_upgrade script
+        self.post_upgrade_file_name = f"/home/{upgrade_info['ipv4_address']}/post_upgrade.sh"
+        self.post_upgrade_blob_name = f"{upgrade_info['manufacturer']}/{upgrade_info['model']}/{upgrade_info['target_firmware_version']}/post_upgrade.sh"
         super().__init__(upgrade_info)
 
     def upgrade(self):
@@ -34,9 +39,6 @@ class CommsigniaUpgrader(upgrader.UpgraderAbstractClass):
             scp.put(self.local_file_name, remote_path="/tmp/")
             scp.close()
 
-            # Delete local installation package and its parent directory so it doesn't take up storage space
-            self.cleanup()
-
             # Run firmware upgrade and reboot
             logging.info("Running firmware upgrade...")
             _stdin, _stdout, _stderr = ssh.exec_command(
@@ -52,6 +54,13 @@ class CommsigniaUpgrader(upgrader.UpgraderAbstractClass):
             ssh.exec_command("reboot")
             ssh.close()
 
+            # In here check to see if post_upgrade is present
+            if (self.download_blob(self.post_upgrade_blob_name, self.post_upgrade_file_name)):
+                self.post_upgrade()
+
+            # Delete local installation package and its parent directory so it doesn't take up storage space
+            self.cleanup()
+
             # Notify Firmware Manager of successful firmware upgrade completion
             self.notify_firmware_manager(success=True)
         except Exception as err:
@@ -60,6 +69,61 @@ class CommsigniaUpgrader(upgrader.UpgraderAbstractClass):
             self.cleanup()
             self.notify_firmware_manager(success=False)
 
+    def post_upgrade(self):
+        if self.wait_until_online() == -1:
+            raise Exception("RSU offline for too long after firmware upgrade")
+        try:
+            time.sleep(60)
+            # Make connection with the target device
+            logging.info("Making SSH connection with the device...")
+            ssh = SSHClient()
+            ssh.set_missing_host_key_policy(WarningPolicy)
+            ssh.connect(
+                self.rsu_ip,
+                username=self.ssh_username,
+                password=self.ssh_password,
+                look_for_keys=False,
+                allow_agent=False,
+            )
+
+            # Make SCP client to copy over the post upgrade script s to the /tmp/ directory on the remote device
+            logging.info("Copying post upgrade script to the device...")
+            scp = SCPClient(ssh.get_transport())
+            scp.put(self.post_upgrade_file_name, remote_path="/tmp/")
+            scp.close()
+
+            # Run post upgrade
+            logging.info("Running post upgrade script...")
+            ssh.exec_command(
+                f"chmod +x /tmp/post_upgrade.sh"
+            )
+            _stdin, _stdout, _stderr = ssh.exec_command(
+                f"/tmp/post_upgrade.sh"
+            )
+            decoded_stdout = _stdout.read().decode()
+            logging.info(decoded_stdout)
+            if "ALL OK" not in decoded_stdout:
+                ssh.close()
+                logging.error(f"Failed to execute post upgrade script for rsu {self.rsu_ip}: {decoded_stdout}")
+                return
+            ssh.close()
+            logging.info(f"Post upgrade script executed successfully for rsu: {self.rsu_ip}.")
+        except Exception as err:
+            logging.error(f"Failed to execute post upgrade script for rsu {self.rsu_ip}: {err}")
+            
+    def wait_until_online(self):
+        iter = 0
+        # Ping once every second for 3 minutes until online
+        while iter < 180:
+            time.sleep(1)
+            code = subprocess.run(
+                ["ping", "-n", "-c1", self.rsu_ip], capture_output=True
+            ).returncode
+            if code == 0:
+                return 0
+            iter += 1
+        # 3 minutes pass with no response
+        return -1
 
 # sys.argv[1] - JSON string with the following key-values:
 # - ipv4_address

691f8b46b553a92997f34eb5efada98f30ffc27f
dmccoystephenson <dmccoystephenson@gmail.com>
2024-04-25 06:56:50 -0600

    Replaced single-line comments with docstrings in `download_blob.py`

--- services/addons/images/firmware_manager/download_blob.py
+++ services/addons/images/firmware_manager/download_blob.py
@@ -3,8 +3,14 @@ import logging
 import os
 
 
-# Download a blob from GCP Bucket Storage
 def download_gcp_blob(blob_name, destination_file_name):
+    """Download a file from a GCP Bucket Storage bucket to a local file.
+
+    Args:
+        blob_name (str): The name of the file in the bucket.
+        destination_file_name (str): The name of the local file to download the bucket file to.
+    """
+
     gcp_project = os.environ.get("GCP_PROJECT")
     bucket_name = os.environ.get("BLOB_STORAGE_BUCKET")
     storage_client = storage.Client(gcp_project)
@@ -16,8 +22,14 @@ def download_gcp_blob(blob_name, destination_file_name):
     )
 
 
-# "Download" a blob from a directory mounted as a volume in a Docker container
 def download_docker_blob(blob_name, destination_file_name):
+    """Copy a file from a directory mounted as a volume in a Docker container to a local file.
+
+    Args:
+        blob_name (str): The name of the file in the directory.
+        destination_file_name (str): The name of the local file to copy the directory file to.
+    """
+    
     directory = "/mnt/blob_storage"
     source_file_name = f"{directory}/{blob_name}"
     os.system(f"cp {source_file_name} {destination_file_name}")

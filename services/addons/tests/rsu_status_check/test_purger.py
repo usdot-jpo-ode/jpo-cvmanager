5f342ad9c5ea560ccb58e219d168e78b4485f877
Drew Johnston <31270488+drewjj@users.noreply.github.com>
2024-04-26 14:42:44 -0600

    Unit tests updated for the changes made

--- services/addons/tests/rsu_status_check/test_purger.py
+++ services/addons/tests/rsu_status_check/test_purger.py
@@ -4,34 +4,54 @@ from addons.images.rsu_status_check import purger
 from freezegun import freeze_time
 
 
-@freeze_time("2023-07-06")
+@patch("addons.images.rsu_status_check.purger.pgquery.query_db")
+def test_get_all_rsus(mock_query_db):
+    # mock
+    mock_query_db.return_value = [
+        ({"rsu_id": 1},),
+        ({"rsu_id": 2},),
+        ({"rsu_id": 3},),
+        ({"rsu_id": 4},),
+    ]
+
+    # call
+    result = purger.get_all_rsus()
+
+    # check
+    assert 1 in result and result[1] is None
+    assert 2 in result and result[2] is None
+    assert 3 in result and result[3] is None
+    assert 4 in result and result[4] is None
+    assert 5 not in result
+
+
 @patch("addons.images.rsu_status_check.purger.pgquery.query_db")
 def test_get_last_online_rsu_records(mock_query_db):
     # mock
-    mock_query_db.return_value = [(1, 1, datetime.now())]
+    mock_query_db.return_value = [
+        ({"ping_id": 1, "rsu_id": 1, "timestamp": "2023-07-06T00:00:00"},),
+    ]
+    rsu_dict = {1: None}
 
     # call
-    result = purger.get_last_online_rsu_records()
+    result = purger.get_last_online_rsu_records(rsu_dict)
 
     # check
     assert len(result) == 1
-    assert result[0][0] == 1
-    assert result[0][1] == 1
-    assert result[0][2].strftime("%Y/%m/%d") == "2023/07/06"
+    assert result[1]["ping_id"] == 1
+    assert result[1]["timestamp"].strftime("%Y/%m/%d") == "2023/07/06"
 
 
 @freeze_time("2023-07-06")
+@patch("addons.images.rsu_status_check.purger.get_all_rsus", MagicMock())
+@patch("addons.images.rsu_status_check.purger.get_last_online_rsu_records")
 @patch("addons.images.rsu_status_check.purger.pgquery.write_db")
-def test_purge_ping_data(mock_write_db):
+def test_purge_ping_data(mock_write_db, mock_glorr):
     now_dt = datetime.now()
-    purger.get_last_online_rsu_records = MagicMock(
-        return_value=[
-            [0, 0, now_dt - timedelta(hours=10)],
-            [1, 1, now_dt - timedelta(days=3)],
-        ]
-    )
-    purger.logging.info = MagicMock()
-    purger.logging.debug = MagicMock()
+    mock_glorr.return_value = {
+        1: {"ping_id": 1, "timestamp": now_dt - timedelta(hours=10)},
+        2: {"ping_id": 2, "timestamp": now_dt - timedelta(days=3)},
+    }
 
     purger.purge_ping_data(24)
 
@@ -39,24 +59,21 @@ def test_purge_ping_data(mock_write_db):
     mock_write_db.assert_has_calls(
         [
             call(
-                "DELETE FROM public.ping WHERE rsu_id = 0 AND timestamp < '2023/07/05T00:00:00'::timestamp"
+                "DELETE FROM public.ping WHERE rsu_id = 1 AND timestamp < '2023-07-05T00:00:00'::timestamp"
             ),
-            call("DELETE FROM public.ping WHERE rsu_id = 1 AND ping_id != 1"),
+            call("DELETE FROM public.ping WHERE rsu_id = 2 AND ping_id != 2"),
         ]
     )
-    purger.logging.info.assert_called_once()
 
 
 @freeze_time("2023-07-06")
+@patch("addons.images.rsu_status_check.purger.get_all_rsus", MagicMock())
+@patch("addons.images.rsu_status_check.purger.get_last_online_rsu_records")
 @patch("addons.images.rsu_status_check.purger.pgquery.write_db")
-def test_purge_ping_data_none(mock_write_db):
-    now_dt = datetime.now()
-    purger.get_last_online_rsu_records = MagicMock(return_value=[])
-    purger.logging.info = MagicMock()
-    purger.logging.debug = MagicMock()
+def test_purge_ping_data_none(mock_write_db, mock_glorr):
+    mock_glorr.return_value = {}
 
     purger.purge_ping_data(24)
 
     purger.get_last_online_rsu_records.assert_called_once()
     mock_write_db.assert_not_called()
-    purger.logging.info.assert_called_once()

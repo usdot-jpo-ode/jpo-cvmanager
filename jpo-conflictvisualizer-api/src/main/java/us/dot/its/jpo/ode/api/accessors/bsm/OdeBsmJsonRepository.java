7fd4eeb2c85038534cffb53590398b35504f07cf
John-Wiens <jwiens3141@gmail.com>
2024-04-29 16:08:57 -0600

    Added Endpoint to get BSM message Counts

--- jpo-conflictvisualizer-api/src/main/java/us/dot/its/jpo/ode/api/accessors/bsm/OdeBsmJsonRepository.java
+++ jpo-conflictvisualizer-api/src/main/java/us/dot/its/jpo/ode/api/accessors/bsm/OdeBsmJsonRepository.java
@@ -6,5 +6,6 @@ import us.dot.its.jpo.ode.api.models.DataLoader;
 import us.dot.its.jpo.ode.model.OdeBsmData;
 
 public interface OdeBsmJsonRepository extends DataLoader<OdeBsmData>{
-    List<OdeBsmData> findOdeBsmDataGeo(String originIp, String vehicleId, Long startTime, Long endTime, Double longitude, Double latitude, Double distance);  
+    List<OdeBsmData> findOdeBsmDataGeo(String originIp, String vehicleId, Long startTime, Long endTime, Double longitude, Double latitude, Double distance);
+    long countOdeBsmDataGeo(String originIp, String vehicleId, Long startTime, Long endTime, Double longitude, Double latitude, Double distance); 
 }

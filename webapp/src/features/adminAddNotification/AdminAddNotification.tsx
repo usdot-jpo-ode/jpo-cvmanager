f974f52e2ffd1762a9a59149dfbfbafbcff6ed72
Marc Wodahl <mwodahl@trihydro.com>
2024-06-19 11:25:43 -0600

    Admin Add Notification

---
+++ webapp/src/features/adminAddNotification/AdminAddNotification.tsx
@@ -0,0 +1,106 @@
+import React, { useEffect } from 'react'
+import { Form } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
+import { DropdownList } from 'react-widgets'
+import {
+  selectSuccessMsg,
+  selectSelectedType,
+  selectAvailableTypes,
+  selectApiData,
+  selectErrorState,
+  selectErrorMsg,
+  selectSubmitAttempt,
+
+  // actions
+  submitForm,
+  AdminNotificationForm,
+  setSelectedType,
+  updateEmailTypesApiData,
+  getNotificationData,
+} from './adminAddNotificationSlice'
+import { selectEmail } from '../../generalSlices/userSlice'
+import { useSelector, useDispatch } from 'react-redux'
+
+import '../adminRsuTab/Admin.css'
+import 'react-widgets/styles.css'
+import { AnyAction, ThunkDispatch } from '@reduxjs/toolkit'
+import { RootState } from '../../store'
+
+const AdminAddNotification = () => {
+  const dispatch: ThunkDispatch<RootState, void, AnyAction> = useDispatch()
+  const successMsg = useSelector(selectSuccessMsg)
+  const apiData = useSelector(selectApiData)
+  const errorState = useSelector(selectErrorState)
+  const errorMsg = useSelector(selectErrorMsg)
+  const submitAttempt = useSelector(selectSubmitAttempt)
+  const selectedType = useSelector(selectSelectedType)
+  const availableTypes = useSelector(selectAvailableTypes)
+  const userEmail = useSelector(selectEmail)
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<AdminNotificationForm>()
+
+  useEffect(() => {
+    dispatch(getNotificationData())
+  }, [dispatch])
+
+  useEffect(() => {
+    dispatch(updateEmailTypesApiData())
+  }, [apiData, dispatch])
+
+  const onSubmit = (data: AdminNotificationForm) => dispatch(submitForm({ data, reset }))
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" defaultValue={userEmail} value={userEmail} {...register('email')} readOnly />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="email_type">
+          <Form.Label>Email Notification</Form.Label>
+          <DropdownList
+            className="form-dropdown"
+            dataKey="type"
+            textField="type"
+            placeholder="Select email notification (Required)"
+            data={availableTypes}
+            value={selectedType}
+            onChange={(value) => {
+              dispatch(setSelectedType(value))
+            }}
+          />
+        </Form.Group>
+
+        {selectedType.type === '' && submitAttempt && (
+          <p className="error-msg" role="alert">
+            Must select at least one email notification type
+          </p>
+        )}
+
+        {successMsg && (
+          <p className="success-msg" role="status">
+            {successMsg}
+          </p>
+        )}
+        {errorState && (
+          <p className="error-msg" role="alert">
+            Failed to add email notification due to error: {errorMsg}
+          </p>
+        )}
+        <div className="form-control">
+          <label></label>
+          <button type="submit" className="admin-button">
+            Add Email Notification
+          </button>
+        </div>
+      </Form>
+    </div>
+  )
+}
+
+export default AdminAddNotification

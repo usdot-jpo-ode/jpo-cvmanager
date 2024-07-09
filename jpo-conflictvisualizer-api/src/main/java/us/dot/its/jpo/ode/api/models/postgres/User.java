96d84a9563c3086f61c659d8bccdb8d6c7625476
john-wiens <jwiens3141@gmail.com>
2024-07-09 14:10:48 -0600

    Added basic postgres query capability

---
+++ jpo-conflictvisualizer-api/src/main/java/us/dot/its/jpo/ode/api/models/postgres/User.java
@@ -0,0 +1,31 @@
+package us.dot.its.jpo.ode.api.models.postgres;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@ToString
+@Setter
+@EqualsAndHashCode
+@Getter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private UUID user_id;
+    private String email;
+    private String first_name;
+    private String last_name;
+    private long created_timestamp;
+    private boolean super_user;
+    private boolean receive_error_emails;
+    
+}

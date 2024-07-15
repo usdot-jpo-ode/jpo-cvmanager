13691c789633ad6151190ee15401ad8b5627cec6
john-wiens <jwiens3141@gmail.com>
2024-07-15 16:46:37 -0600

    Updated BSM, SPaT, Map Unit Tests

--- jpo-conflictvisualizer-api/src/test/java/us/dot/its/jpo/ode/api/MapTest.java
+++ jpo-conflictvisualizer-api/src/test/java/us/dot/its/jpo/ode/api/MapTest.java
@@ -3,6 +3,7 @@ package us.dot.its.jpo.ode.api;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -20,6 +21,8 @@ import us.dot.its.jpo.geojsonconverter.pojos.geojson.LineString;
 import us.dot.its.jpo.geojsonconverter.pojos.geojson.map.ProcessedMap;
 import us.dot.its.jpo.ode.api.accessors.map.ProcessedMapRepository;
 import us.dot.its.jpo.ode.api.controllers.MapController;
+import us.dot.its.jpo.ode.api.models.postgres.derived.UserOrgRole;
+import us.dot.its.jpo.ode.api.services.PostgresService;
 import us.dot.its.jpo.ode.mockdata.MockMapGenerator;
 
 
@@ -32,13 +35,21 @@ public class MapTest {
 
   @MockBean
   ProcessedMapRepository processedMapRepo;
-    
+
+  @MockBean
+  PostgresService postgresService;
   
 
   @Test
   public void testProcessedMap() {
 
-    MockKeyCloakAuth.setSecurityContextHolder("cm_user", Set.of("USER"));
+    MockKeyCloakAuth.setSecurityContextHolder("cm_user@cimms.com", Set.of("USER"));
+
+    List <UserOrgRole> roles = new ArrayList<>();
+    UserOrgRole userOrgRole = new UserOrgRole("cm_user@cimms.com", "test", "USER");
+
+    roles.add(userOrgRole);
+    when(postgresService.findUserOrgRoles("cm_user@cimms.com")).thenReturn(roles);
 
 
     List<ProcessedMap<LineString>> list = MockMapGenerator.getProcessedMaps();
--- jpo-conflictvisualizer-api/src/test/java/us/dot/its/jpo/ode/api/SpatTest.java
+++ jpo-conflictvisualizer-api/src/test/java/us/dot/its/jpo/ode/api/SpatTest.java
@@ -3,6 +3,7 @@ package us.dot.its.jpo.ode.api;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -19,6 +20,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import us.dot.its.jpo.geojsonconverter.pojos.spat.ProcessedSpat;
 import us.dot.its.jpo.ode.api.accessors.spat.ProcessedSpatRepository;
 import us.dot.its.jpo.ode.api.controllers.SpatController;
+import us.dot.its.jpo.ode.api.models.postgres.derived.UserOrgRole;
+import us.dot.its.jpo.ode.api.services.PostgresService;
 import us.dot.its.jpo.ode.mockdata.MockSpatGenerator;
 
 
@@ -32,12 +35,19 @@ public class SpatTest {
   @MockBean
   ProcessedSpatRepository processedSpatRepo;
     
-
+  @MockBean
+  PostgresService postgresService;
 
   @Test
   public void testProcessedSpat() {
 
-    MockKeyCloakAuth.setSecurityContextHolder("cm_user", Set.of("USER"));
+    MockKeyCloakAuth.setSecurityContextHolder("cm_user@cimms.com", Set.of("USER"));
+
+    List <UserOrgRole> roles = new ArrayList<>();
+    UserOrgRole userOrgRole = new UserOrgRole("cm_user@cimms.com", "test", "USER");
+
+    roles.add(userOrgRole);
+    when(postgresService.findUserOrgRoles("cm_user@cimms.com")).thenReturn(roles);
 
     List<ProcessedSpat> list = MockSpatGenerator.getProcessedSpats();
     

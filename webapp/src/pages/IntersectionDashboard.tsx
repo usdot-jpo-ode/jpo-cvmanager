b80ab9851da62b547b081813f77737a1c3f7e741
jacob6838 <jfrye@neaeraconsulting.com>
2024-07-27 04:42:18 -0600

    re-installing packages

--- webapp/src/pages/IntersectionDashboard.tsx
+++ webapp/src/pages/IntersectionDashboard.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { updateTableData as updateRsuTableData } from '../features/adminRsuTab/adminRsuTabSlice'
 import { getAvailableUsers } from '../features/adminUserTab/adminUserTabSlice'
 
@@ -15,9 +15,22 @@ import DataSelectorPage from '../components/intersections/DataSelectorPage'
 import ConfigurationPage from '../components/intersections/ConfigurationPage'
 import ReportsPage from '../components/intersections/ReportsPage'
 import DecoderPage from '../components/intersections/DecoderPage'
+import { InputLabel, Select, MenuItem, IconButton } from '@mui/material'
+import { Tooltip, FormControl } from 'react-bootstrap'
+import { setOpenMapDialog } from '../features/intersections/data-selector/dataSelectorSlice'
+import {
+  selectIntersections,
+  selectSelectedIntersectionId,
+  setSelectedIntersection,
+} from '../generalSlices/intersectionSlice'
+import MapIconRounded from '@mui/icons-material/Map'
+import MapDialog from '../features/intersections/intersection-selector/intersection-selector-dialog'
 
 function IntersectionDashboard() {
   const dispatch: ThunkDispatch<RootState, void, AnyAction> = useDispatch()
+  const intersectionId = useSelector(selectSelectedIntersectionId)
+  const intersections = useSelector(selectIntersections)
+  const [openMapDialog, setOpenMapDialog] = useState(false)
 
   useEffect(() => {
     dispatch(updateRsuTableData())
@@ -28,6 +41,43 @@ function IntersectionDashboard() {
     <>
       <div id="admin">
         <h2 className="adminHeader">CV Manager Admin Interface</h2>
+        <Tooltip title="Select Intersection">
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Intersection ID</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={intersectionId}
+              label="IntersectionId"
+              onChange={(e) => {
+                setSelectedIntersection(e.target.value as number | undefined)
+              }}
+            >
+              <MenuItem value={-1} key={-1}>
+                No Intersection
+              </MenuItem>
+              {intersections.map((intersection) => {
+                return (
+                  <MenuItem value={intersection?.intersectionID} key={intersection?.intersectionID}>
+                    {intersection?.intersectionID == -1
+                      ? 'No Intersection'
+                      : intersection?.intersectionID +
+                        (intersection?.intersectionName ? ': ' + intersection?.intersectionName : '')}
+                  </MenuItem>
+                )
+              })}
+            </Select>
+          </FormControl>
+        </Tooltip>
+        <Tooltip title="Select Intersection on Map">
+          <IconButton
+            onClick={() => {
+              setOpenMapDialog(true)
+            }}
+          >
+            <MapIconRounded fontSize="large" />
+          </IconButton>
+        </Tooltip>
         <VerticalTabs
           notFoundRoute={
             <NotFound
@@ -58,11 +108,12 @@ function IntersectionDashboard() {
               title: 'Data Selector',
               child: <DataSelectorPage />,
             },
-            {
-              path: 'decoder',
-              title: 'Decoder',
-              child: <DecoderPage />,
-            },
+            // The decoder page is still under development
+            // {
+            //   path: 'decoder',
+            //   title: 'Decoder',
+            //   child: <DecoderPage />,
+            // },
             {
               path: 'reports',
               title: 'Reports',
@@ -76,6 +127,13 @@ function IntersectionDashboard() {
           ]}
         />
       </div>
+      <MapDialog
+        open={openMapDialog}
+        onClose={() => {
+          setOpenMapDialog(false)
+        }}
+        intersections={intersections.filter((v) => v?.intersectionID != undefined) as IntersectionReferenceData[]}
+      />
     </>
   )
 }

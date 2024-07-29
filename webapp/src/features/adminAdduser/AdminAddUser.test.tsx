5556f70d05a60075cf7bda1f1c46ce1894fdc8bb
Marc Wodahl <mwodahl@trihydro.com>
2024-07-29 12:17:09 -0600

    Testing Updates

--- webapp/src/features/adminAdduser/AdminAddUser.test.tsx
+++ webapp/src/features/adminAdduser/AdminAddUser.test.tsx
@@ -4,11 +4,16 @@ import AdminAddUser from './AdminAddUser'
 import { Provider } from 'react-redux'
 import { setupStore } from '../../store'
 import { replaceChaoticIds } from '../../utils/test-utils'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 it('should take a snapshot', () => {
   const { container } = render(
     <Provider store={setupStore({})}>
-      <AdminAddUser />
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<AdminAddUser />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   )
 

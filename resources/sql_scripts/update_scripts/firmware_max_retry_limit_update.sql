294d872db7ea1f259b0ab7a471a583d9fac97015
dmccoystephenson <dmccoystephenson@gmail.com>
2024-08-13 11:39:07 -0600

    Added migration script for new tables

---
+++ resources/sql_scripts/update_scripts/firmware_max_retry_limit_update.sql
@@ -0,0 +1,23 @@
+-- Run this SQL update script if you already have a deployed CV Manager PostgreSQL database
+
+CREATE TABLE IF NOT EXISTS public.consecutive_firmware_upgrade_failures
+(
+   rsu_id integer NOT NULL,
+   consecutive_failures integer NOT NULL,
+   CONSTRAINT consecutive_firmware_upgrade_failures_pkey PRIMARY KEY (rsu_id),
+   CONSTRAINT fk_rsu_id FOREIGN KEY (rsu_id)
+      REFERENCES public.rsus (rsu_id) MATCH SIMPLE
+      ON UPDATE NO ACTION
+      ON DELETE NO ACTION
+);
+
+CREATE TABLE IF NOT EXISTS public.max_retry_limit_reached_instances
+(
+   rsu_id integer NOT NULL,
+   reached_at timestamp without time zone NOT NULL,
+   CONSTRAINT max_retry_limit_reached_instances_pkey PRIMARY KEY (rsu_id, reached_at),
+   CONSTRAINT fk_rsu_id FOREIGN KEY (rsu_id)
+      REFERENCES public.rsus (rsu_id) MATCH SIMPLE
+      ON UPDATE NO ACTION
+      ON DELETE NO ACTION
+);

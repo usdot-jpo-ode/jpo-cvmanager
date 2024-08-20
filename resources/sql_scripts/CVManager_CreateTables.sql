5097d4009d1e7c7a3d73fc93048578331686a4da
Drew Johnston <31270488+drewjj@users.noreply.github.com>
2024-08-20 03:00:42 -0600

    Counter organization email support

--- resources/sql_scripts/CVManager_CreateTables.sql
+++ resources/sql_scripts/CVManager_CreateTables.sql
@@ -427,38 +427,10 @@ CREATE TABLE IF NOT EXISTS public.user_email_notification
 (
    user_email_notification_id integer NOT NULL DEFAULT nextval('user_email_notification_user_email_notification_id_seq'::regclass),
    user_id integer NOT NULL,
-   organization_id integer NOT NULL,
    email_type_id integer NOT NULL,
    CONSTRAINT user_email_notification_pkey PRIMARY KEY (user_email_notification_id),
    CONSTRAINT fk_user_id FOREIGN KEY (user_id)
       REFERENCES public.users (user_id) MATCH SIMPLE
-      ON UPDATE NO ACTION
-      ON DELETE NO ACTION,
-   CONSTRAINT fk_organization_id FOREIGN KEY (organization_id)
-      REFERENCES public.organizations (organization_id) MATCH SIMPLE
-      ON UPDATE NO ACTION
-      ON DELETE NO ACTION,
-   CONSTRAINT fk_email_type_id FOREIGN KEY (email_type_id)
-      REFERENCES public.email_type (email_type_id) MATCH SIMPLE
-      ON UPDATE NO ACTION
-      ON DELETE NO ACTION
-);
-
-CREATE SEQUENCE public.organization_email_notification_organization_email_notification_id_seq
-   INCREMENT 1
-   START 1
-   MINVALUE 1
-   MAXVALUE 2147483647
-   CACHE 1;
-
-CREATE TABLE IF NOT EXISTS public.organization_email_notification
-(
-   organization_email_notification_id integer NOT NULL DEFAULT nextval('organization_email_notification_organization_email_notification_id_seq'::regclass),
-   organization_id integer NOT NULL,
-   email_type_id integer NOT NULL,
-   CONSTRAINT organization_email_notification_id_pkey PRIMARY KEY (organization_email_notification_id),
-   CONSTRAINT fk_organization_id FOREIGN KEY (organization_id)
-      REFERENCES public.organizations (organization_id) MATCH SIMPLE
       ON UPDATE NO ACTION
       ON DELETE NO ACTION,
    CONSTRAINT fk_email_type_id FOREIGN KEY (email_type_id)
--- services/common/email_util.py
+++ services/common/email_util.py
@@ -1,16 +1,64 @@
 from common.pgquery import query_db
 
-def get_email_list(msg_type):
-    email_list = []
+
+def build_user_email_list(msg_type, org_name):
+    # Build the user email query based on the availability of an organization name
     email_query = (
-        "SELECT to_jsonb(row) FROM (SELECT email FROM public.users WHERE user_id IN "
-        "(SELECT user_id FROM public.user_email_notification WHERE email_type_id = "
-        f"(SELECT email_type_id FROM email_type WHERE email_type = '{msg_type}'))) as row"
+        "SELECT to_jsonb(row) FROM ("
+        "SELECT email FROM public.users "
+        "WHERE user_id IN ("
+        "SELECT user_id FROM public.user_email_notification "
+        "WHERE email_type_id = ("
+        f"SELECT email_type_id FROM public.email_type WHERE email_type = '{msg_type}'"
+        "))"
     )
+    if org_name:
+        email_query += (
+            " AND user_id IN ("
+            "SELECT user_id FROM public.user_organization "
+            "WHERE organization_id = ("
+            f"SELECT organization_id FROM public.organizations WHERE name = '{org_name}'"
+            "))"
+        )
+    email_query += ") as row"
 
     data = query_db(email_query)
+
+    email_list = []
     for row in data:
         row = dict(row[0])
         email_list.append(row["email"])
+    return email_list
+
+
+def build_org_email_list(org_name):
+    email_query = (
+        "SELECT to_jsonb(row) FROM ("
+        f"SELECT email FROM public.organizations WHERE name = '{org_name}'"
+        ") as row"
+    )
+
+    data = query_db(email_query)
+
+    email_list = []
+    for row in data:
+        row = dict(row[0])
+        email = row["email"]
+        if email:
+            if email.strip() != "":
+                email_list.append(row["email"])
+    return email_list
+
+
+def get_email_list(msg_type, org_name=None):
+    email_list = []
+
+    # Verify presence of organization email if org_name is provided
+    if org_name:
+        email_list = build_org_email_list(org_name)
+
+    # Generate the email list based off of user email preferences if no organization email is configured
+    if len(email_list) == 0:
+        email_list = build_user_email_list(msg_type, org_name)
 
     return email_list

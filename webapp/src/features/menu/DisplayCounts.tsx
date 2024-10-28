b20c4f094894e916d0121cae642cd51d7aa21dbc
jacob6838 <jfrye@neaeraconsulting.com>
2024-10-28 17:27:59 -0600

    Updating map counts layer

--- webapp/src/features/menu/DisplayCounts.tsx
+++ webapp/src/features/menu/DisplayCounts.tsx
@@ -8,7 +8,6 @@ import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker'
 import TextField from '@mui/material/TextField'
 import EnvironmentVars from '../../EnvironmentVars'
 import BounceLoader from 'react-spinners/BounceLoader'
-import Select from 'react-select'
 import {
   selectRequestOut,
   selectMsgType,
@@ -26,6 +25,7 @@ import { AnyAction, ThunkDispatch } from '@reduxjs/toolkit'
 import { RootState } from '../../store'
 import { CountsListElement } from '../../models/Rsu'
 import { MessageType } from '../../models/MessageTypes'
+import { MenuItem, Select, Typography, useTheme } from '@mui/material'
 
 const messageTypeOptions = EnvironmentVars.getMessageTypes().map((type) => {
   return { value: type, label: type }
@@ -33,6 +33,7 @@ const messageTypeOptions = EnvironmentVars.getMessageTypes().map((type) => {
 
 const DisplayCounts = () => {
   const dispatch: ThunkDispatch<RootState, void, AnyAction> = useDispatch()
+  const theme = useTheme()
   const countsMsgType = useSelector(selectMsgType)
   const startDate = useSelector(selectStartDate)
   const endDate = useSelector(selectEndDate)
@@ -51,9 +52,13 @@ const DisplayCounts = () => {
 
   const getWarningMessage = (warning: boolean) =>
     warning ? (
-      <span className="warningMessage" role="alert">
-        <p>Warning: time ranges greater than 24 hours may have longer load times.</p>
-      </span>
+      <Typography
+        component="span"
+        role="alert"
+        sx={{ backgroundColor: theme.palette.error.main, display: 'flex', justifyContent: 'center' }}
+      >
+        Warning: time ranges greater than 24 hours may have longer load times.
+      </Typography>
     ) : (
       <span></span>
     )
@@ -73,15 +78,21 @@ const DisplayCounts = () => {
           </div>
         </div>
         <span className="bounceLoader">
-          <BounceLoader loading={true} color={'#ffffff'}></BounceLoader>
+          <BounceLoader loading={true} color={theme.palette.text.primary}></BounceLoader>
         </span>
       </div>
     ) : (
       <div className="table">
         <div className="header">
-          <div onClick={() => sortBy('rsu')}>RSU</div>
-          <div onClick={() => sortBy('road')}>Road</div>
-          <div onClick={() => sortBy('count')}>Count</div>
+          <div onClick={() => sortBy('rsu')} style={{ border: `1px solid ${theme.palette.text.primary}` }}>
+            RSU
+          </div>
+          <div onClick={() => sortBy('road')} style={{ border: `1px solid ${theme.palette.text.primary}` }}>
+            Road
+          </div>
+          <div onClick={() => sortBy('count')} style={{ border: `1px solid ${theme.palette.text.primary}` }}>
+            Count
+          </div>
         </div>
         <div className="body">{formatRows(sortedCountList)}</div>
       </div>
@@ -91,7 +102,7 @@ const DisplayCounts = () => {
     <div>
       <div id="container" className="sideBarOn">
         <h1 className="h1">{countsMsgType} Counts</h1>
-        <div className="DateRangeContainer">
+        <div className="DateRangeContainer" style={{ border: `2px solid ${theme.palette.text.primary}` }}>
           <div style={{ marginBottom: '8px' }}>
             <LocalizationProvider dateAdapter={AdapterDateFns}>
               <DateTimePicker
@@ -121,23 +132,40 @@ const DisplayCounts = () => {
           </div>
         </div>
         <Select
-          options={messageTypeOptions}
-          defaultValue={messageTypeOptions.filter((o) => o.label === countsMsgType)}
           placeholder="Select Message Type"
-          className="selectContainer"
-          onChange={(value) => dispatch(updateMessageType(value.value as MessageType))}
-        />
+          value={countsMsgType}
+          onChange={(event) => dispatch(updateMessageType(event.target.value as MessageType))}
+          sx={{
+            width: '90%',
+            textAlign: 'center',
+            marginLeft: 2.5,
+            marginRight: 'auto',
+            position: 'relative',
+            zIndex: 1000,
+          }}
+        >
+          {messageTypeOptions.map((option) => {
+            return (
+              <MenuItem value={option.value} key={option.value}>
+                {option.label}
+              </MenuItem>
+            )
+          })}
+        </Select>
         {getWarningMessage(warning)}
         {getTable(messageLoading, sortedCountList)}
       </div>
     </div>
   )
 }
-const Row = ({ rsu, road, count }: { rsu: string; road: string; count: number }) => (
-  <div className="row">
-    <div>{rsu}</div>
-    <div>{road}</div>
-    <div>{count}</div>
-  </div>
-)
+const Row = ({ rsu, road, count }: { rsu: string; road: string; count: number }) => {
+  const theme = useTheme()
+  return (
+    <div className="row">
+      <div style={{ border: `1px solid ${theme.palette.text.primary}` }}>{rsu}</div>
+      <div style={{ border: `1px solid ${theme.palette.text.primary}` }}>{road}</div>
+      <div style={{ border: `1px solid ${theme.palette.text.primary}` }}>{count}</div>
+    </div>
+  )
+}
 export default DisplayCounts

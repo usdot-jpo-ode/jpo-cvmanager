249c9e464e6e67fdb8da21cd806503cbb69f6394
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2024-12-17 16:22:30 -0700

    Removed unused code

--- jpo-conflictvisualizer-api/src/main/java/us/dot/its/jpo/ode/api/APIServiceController.java
+++ jpo-conflictvisualizer-api/src/main/java/us/dot/its/jpo/ode/api/APIServiceController.java
@@ -1,7 +1,5 @@
 package us.dot.its.jpo.ode.api;
 
-import java.util.ArrayList;
-
 import java.util.List;
 
 
@@ -11,63 +9,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
-import org.springframework.kafka.core.KafkaAdmin;
 
 import org.springframework.stereotype.Controller;
 
-import us.dot.its.jpo.conflictmonitor.monitor.models.assessments.ConnectionOfTravelAssessment;
-import us.dot.its.jpo.conflictmonitor.monitor.models.assessments.LaneDirectionOfTravelAssessment;
-import us.dot.its.jpo.conflictmonitor.monitor.models.assessments.StopLinePassageAssessment;
-import us.dot.its.jpo.conflictmonitor.monitor.models.bsm.BsmEvent;
-import us.dot.its.jpo.conflictmonitor.monitor.models.events.ConnectionOfTravelEvent;
-import us.dot.its.jpo.conflictmonitor.monitor.models.events.IntersectionReferenceAlignmentEvent;
-import us.dot.its.jpo.conflictmonitor.monitor.models.events.LaneDirectionOfTravelEvent;
-import us.dot.its.jpo.conflictmonitor.monitor.models.events.SignalGroupAlignmentEvent;
-import us.dot.its.jpo.conflictmonitor.monitor.models.events.SignalStateConflictEvent;
-import us.dot.its.jpo.conflictmonitor.monitor.models.events.TimeChangeDetailsEvent;
-import us.dot.its.jpo.conflictmonitor.monitor.models.notifications.ConnectionOfTravelNotification;
-import us.dot.its.jpo.conflictmonitor.monitor.models.notifications.IntersectionReferenceAlignmentNotification;
-import us.dot.its.jpo.conflictmonitor.monitor.models.notifications.LaneDirectionOfTravelNotification;
-import us.dot.its.jpo.conflictmonitor.monitor.models.notifications.SignalGroupAlignmentNotification;
-import us.dot.its.jpo.conflictmonitor.monitor.models.notifications.SignalStateConflictNotification;
-import us.dot.its.jpo.conflictmonitor.monitor.models.notifications.Notification;
-import us.dot.its.jpo.conflictmonitor.monitor.serialization.JsonSerdes;
-import us.dot.its.jpo.geojsonconverter.pojos.geojson.LineString;
-import us.dot.its.jpo.geojsonconverter.pojos.geojson.map.ProcessedMap;
-import us.dot.its.jpo.geojsonconverter.pojos.spat.ProcessedSpat;
 import us.dot.its.jpo.ode.api.topologies.*;
-import us.dot.its.jpo.ode.model.OdeSpatData;
-import us.dot.its.jpo.ode.model.OdeBsmData;
-import us.dot.its.jpo.ode.model.OdeMapData;
 
-import us.dot.its.jpo.ode.api.accessors.assessments.ConnectionOfTravelAssessment.ConnectionOfTravelAssessmentRepository;
-import us.dot.its.jpo.ode.api.accessors.assessments.LaneDirectionOfTravelAssessment.LaneDirectionOfTravelAssessmentRepository;
-import us.dot.its.jpo.ode.api.accessors.assessments.SignalStateAssessment.StopLineStopAssessmentRepository;
-import us.dot.its.jpo.ode.api.accessors.assessments.SignalStateEventAssessment.SignalStateEventAssessmentRepository;
-import us.dot.its.jpo.ode.api.accessors.bsm.OdeBsmJsonRepository;
-import us.dot.its.jpo.ode.api.accessors.config.DefaultConfig.DefaultConfigRepository;
-import us.dot.its.jpo.ode.api.accessors.config.IntersectionConfig.IntersectionConfigRepository;
-import us.dot.its.jpo.ode.api.accessors.events.BsmEvent.BsmEventRepository;
-import us.dot.its.jpo.ode.api.accessors.events.ConnectionOfTravelEvent.ConnectionOfTravelEventRepository;
-import us.dot.its.jpo.ode.api.accessors.events.IntersectionReferenceAlignmentEvent.IntersectionReferenceAlignmentEventRepository;
-import us.dot.its.jpo.ode.api.accessors.events.LaneDirectionOfTravelEvent.LaneDirectionOfTravelEventRepository;
-import us.dot.its.jpo.ode.api.accessors.events.SignalGroupAlignmentEvent.SignalGroupAlignmentEventRepository;
-import us.dot.its.jpo.ode.api.accessors.events.SignalStateConflictEvent.SignalStateConflictEventRepository;
-import us.dot.its.jpo.ode.api.accessors.events.SignalStateEvent.SignalStateEventRepository;
-import us.dot.its.jpo.ode.api.accessors.events.SignalStateStopEvent.SignalStateStopEventRepository;
-import us.dot.its.jpo.ode.api.accessors.events.TimeChangeDetailsEvent.TimeChangeDetailsEventRepository;
-import us.dot.its.jpo.ode.api.accessors.map.OdeMapDataRepository;
-import us.dot.its.jpo.ode.api.accessors.map.ProcessedMapRepository;
-import us.dot.its.jpo.ode.api.accessors.notifications.ActiveNotification.ActiveNotificationRepository;
-import us.dot.its.jpo.ode.api.accessors.notifications.ConnectionOfTravelNotification.ConnectionOfTravelNotificationRepository;
-import us.dot.its.jpo.ode.api.accessors.notifications.IntersectionReferenceAlignmentNotification.IntersectionReferenceAlignmentNotificationRepository;
-import us.dot.its.jpo.ode.api.accessors.notifications.LaneDirectionOfTravelNotificationRepo.LaneDirectionOfTravelNotificationRepository;
-import us.dot.its.jpo.ode.api.accessors.notifications.MapBroadcastRateNotification.MapBroadcastRateNotificationRepository;
-import us.dot.its.jpo.ode.api.accessors.notifications.SignalGroupAlignmentNotificationRepo.SignalGroupAlignmentNotificationRepository;
-import us.dot.its.jpo.ode.api.accessors.notifications.SignalStateConflictNotification.SignalStateConflictNotificationRepository;
-import us.dot.its.jpo.ode.api.accessors.notifications.SpatBroadcastRateNotification.SpatBroadcastRateNotificationRepository;
-import us.dot.its.jpo.ode.api.accessors.spat.OdeSpatDataRepository;
-import us.dot.its.jpo.ode.api.accessors.spat.ProcessedSpatRepository;
 import us.dot.its.jpo.ode.api.controllers.StompController;
 import lombok.Getter;
 
@@ -88,36 +34,7 @@ public class APIServiceController {
 
     @Autowired
     public APIServiceController(
-            KafkaAdmin admin,
             ConflictMonitorApiProperties props,
-            ProcessedSpatRepository processedSpatRepo,
-            ProcessedMapRepository processedMapRepo,
-            OdeBsmJsonRepository odeBsmJsonRepo,
-            OdeSpatDataRepository odeSpatDataRepo,
-            OdeMapDataRepository odeMapDataRepo,
-            LaneDirectionOfTravelAssessmentRepository laneDirectionOfTravelAssessmentRepo,
-            ConnectionOfTravelAssessmentRepository connectionOfTravelAssessmentRepo,
-            StopLineStopAssessmentRepository signalStateAssessmentRepo,
-            SignalStateEventAssessmentRepository signalStateEventAssessmentRepo,
-            DefaultConfigRepository defaultConfigRepository,
-            IntersectionConfigRepository intersectionConfigRepository,
-            ConnectionOfTravelEventRepository connectionOfTravelEventRepo,
-            IntersectionReferenceAlignmentEventRepository intersectionReferenceAlignmentEventRepo,
-            LaneDirectionOfTravelEventRepository laneDirectionOfTravelEventRepo,
-            SignalGroupAlignmentEventRepository signalGroupAlignmentEventRepo,
-            SignalStateConflictEventRepository signalStateConflictEventRepo,
-            SignalStateStopEventRepository signalStateStopEventRepo,
-            SignalStateEventRepository signalStateEventRepo,
-            TimeChangeDetailsEventRepository timeChangeDetailsEventRepo,
-            IntersectionReferenceAlignmentNotificationRepository intersectionReferenceAlignmentNotificationRepo,
-            LaneDirectionOfTravelNotificationRepository laneDirectionOfTravelNotificationRepo,
-            MapBroadcastRateNotificationRepository mapBroadcastRateNotificationRepo,
-            SignalGroupAlignmentNotificationRepository signalGroupAlignmentNotificationRepo,
-            SignalStateConflictNotificationRepository signalStateConflictNotificationRepo,
-            SpatBroadcastRateNotificationRepository spatBroadcastRateNotificationRepo,
-            ConnectionOfTravelNotificationRepository connectionOfTravelNotificationRepo,
-            BsmEventRepository bsmEventRepo,
-            ActiveNotificationRepository activeNotificationRepo,
             StompController stompController) {
 
         ImmutableList.Builder<RestartableTopology> topologyListBuilder = ImmutableList.builder();

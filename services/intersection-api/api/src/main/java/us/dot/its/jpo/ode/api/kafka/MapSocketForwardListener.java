315e67a131bfbab8dfa8ae7f5fd0d60eefa26a32
jacob6838 <jfrye@neaeraconsulting.com>
2025-01-07 11:18:04 -0700

    Merging conflictvisualizer file additions

---
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/kafka/MapSocketForwardListener.java
@@ -0,0 +1,31 @@
+package us.dot.its.jpo.ode.api.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import us.dot.its.jpo.geojsonconverter.pojos.geojson.LineString;
+import us.dot.its.jpo.geojsonconverter.pojos.geojson.map.ProcessedMap;
+import us.dot.its.jpo.ode.api.controllers.StompController;
+
+@Component
+@Slf4j
+public class MapSocketForwardListener extends BaseSeekToEndListener {
+
+    @Autowired
+    public MapSocketForwardListener(StompController stompController) {
+        super(stompController);
+    }
+
+    @KafkaListener(id = ListenerIds.MAP,
+            idIsGroup = false,
+            topics = "topic.ProcessedMap",
+            concurrency = "1",
+            containerFactory = "mapListenerContainerFactory",
+            autoStartup = "${conflict.monitor.api.kafka-consumers-always-on}")
+    public void listen(ConsumerRecord<String, ProcessedMap<LineString>> record) {
+        stompController.broadcastMap(record.value());
+        log.trace("Received map with offset {}", record.offset());
+    }
+}

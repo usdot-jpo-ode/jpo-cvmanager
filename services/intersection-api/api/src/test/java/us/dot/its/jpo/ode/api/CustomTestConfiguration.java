662b7c060fc4335662e565420067ea34806b02a7
jacob6838 <jfrye@neaeraconsulting.com>
2025-01-20 16:32:04 -0700

    Adding additional excluded services/tasks

--- services/intersection-api/api/src/test/java/us/dot/its/jpo/ode/api/CustomTestConfiguration.java
+++ services/intersection-api/api/src/test/java/us/dot/its/jpo/ode/api/CustomTestConfiguration.java
@@ -4,11 +4,21 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import us.dot.its.jpo.ode.api.services.EmailService;
+import us.dot.its.jpo.ode.api.services.PostgresService;
+import us.dot.its.jpo.ode.api.services.ReportService;
+import us.dot.its.jpo.ode.api.tasks.EmailTask;
+import us.dot.its.jpo.ode.api.tasks.ReportTask;
 
 @TestConfiguration
 @ComponentScan(
     basePackages = "us.dot.its.jpo.ode.api",
-    excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = EmailService.class)
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = EmailService.class),
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = PostgresService.class),
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = ReportService.class),
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = EmailTask.class),
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = ReportTask.class)
+    }
 )
 public class CustomTestConfiguration {
 }

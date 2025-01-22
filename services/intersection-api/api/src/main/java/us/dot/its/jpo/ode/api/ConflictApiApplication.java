a2f6c6c57f0961c94573ae68f8f7470fb0da0c51
jacob6838 <51723791+jacob6838@users.noreply.github.com>
2025-01-22 13:08:25 -0700

    Update services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/ConflictApiApplication.java
    Co-authored-by: Matt Cook <mattheworion.cook@gmail.com>

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/ConflictApiApplication.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/ConflictApiApplication.java
@@ -1,5 +1,6 @@
 package us.dot.its.jpo.ode.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan(basePackages = { "us.dot.its.jpo.ode.api", "us.dot.its.jpo.geojsonconverter.validator" })
+@Slf4j
 public class ConflictApiApplication extends SpringBootServletInitializer {
 
     @Override
@@ -22,8 +24,8 @@ public class ConflictApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ConflictApiApplication.class, args);
-        System.out.println("Started Conflict Monitor API");
-        System.out.println("Conflict Monitor API docs page found here: http://localhost:8089/swagger-ui/index.html");
-        System.out.println("Startup Complete");
+        log.info("Started Conflict Monitor API");
+        log.info("Conflict Monitor API docs page found here: http://localhost:8089/swagger-ui/index.html");
+        log.info("Startup Complete");
     }
 }

5353ae3dc1b5a839c00b48c784dd8f87a342ebed
jacob6838 <jfrye@neaeraconsulting.com>
2025-01-27 15:51:18 -0700

    Converting timestamp parsing to use a map

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/converters/StringToZonedDateTimeConverter.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/converters/StringToZonedDateTimeConverter.java
@@ -5,6 +5,7 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
@@ -18,39 +19,39 @@ import com.mongodb.lang.NonNull;
 public class StringToZonedDateTimeConverter implements Converter<String, ZonedDateTime> {
     private static final Logger logger = LoggerFactory.getLogger(StringToZonedDateTimeConverter.class);
 
-    DateTimeFormatter[] formats = {
+    // Time formats, with Regular Expression patterns to identify time pattern and
+    // DateTimeFormatters to parse the timestamps
+    Map<Pattern, DateTimeFormatter> timeFormatMap = Map.of(
+            Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}[a-zA-Z]+$"),
             DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSVV"),
+            Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{2}[a-zA-Z]+$"),
             DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSVV"),
+            Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{1}[a-zA-Z]+$"),
             DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SVV"),
+            Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}[a-zA-Z]+$"),
             DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssVV"),
-            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSVV")
-    };
-
-    Pattern[] patterns = {
-        Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}[a-zA-Z]+$"),
-        Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{2}[a-zA-Z]+$"),
-        Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{1}[a-zA-Z]+$"),
-        Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}[a-zA-Z]+$"),
-        Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{6}[a-zA-Z]+$"),
-    };
+            Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{6}[a-zA-Z]+$"),
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSVV"));
 
     @Override
     public ZonedDateTime convert(@NonNull String source) {
-        try{
-            for(int i =0; i< patterns.length; i++){
-                if(checkMatch(source, patterns[i])){
-                    return ZonedDateTime.parse(source, formats[i]);
+        try {
+            for (Map.Entry<Pattern, DateTimeFormatter> entry : timeFormatMap.entrySet()) {
+                if (entry.getKey().matcher(source).matches()) {
+                    return ZonedDateTime.parse(source, entry.getValue());
                 }
             }
-            
+
         } catch (DateTimeParseException e) {
-            logger.error("DateTime Parse Exception. Could not parse Timestamp: {} Timestamp may be invalid, or valid parser is missing", source, e);
-        } 
+            logger.error(
+                    "DateTime Parse Exception. Could not parse Timestamp: {} Timestamp may be invalid, or valid parser is missing",
+                    source, e);
+        }
 
         return ZonedDateTime.ofInstant(Instant.EPOCH, ZoneId.of("UTC"));
     }
 
-    public boolean checkMatch(String source, Pattern pattern){
+    public boolean checkMatch(String source, Pattern pattern) {
         return pattern.matcher(source).matches();
     }
 }

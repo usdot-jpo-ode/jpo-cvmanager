1246ead2dab26f77525ef734ec11c1bf5f94318b
john-wiens <jwiens3141@gmail.com>
2025-01-28 11:52:03 -0700

    Fixing String to Zoned Date Time Converter Test

--- services/intersection-api/api/src/test/java/us/dot/its/jpo/ode/api/StringToZonedDateTimeConverterTest.java
+++ services/intersection-api/api/src/test/java/us/dot/its/jpo/ode/api/StringToZonedDateTimeConverterTest.java
@@ -11,40 +11,37 @@ import us.dot.its.jpo.ode.api.converters.StringToZonedDateTimeConverter;
 @SpringBootTest
 public class StringToZonedDateTimeConverterTest {
 
-    String[] inputTimeStrings = {
-        "2025-01-21T14:45:00.123Z",
-        "2025-01-21T14:45:00.12Z",
-        "2025-01-21T14:45:00.1Z",
-        "2025-01-21T14:45:00Z",
-        "2025-01-21T14:45:00.123456Z",
-        "2025/01/21 14:45:00.123456Z"   // Failure Case
-    };
-        
-    ZonedDateTime[] outputZonedDateTimes = {
-        ZonedDateTime.of(2025, 1, 21, 21, 14, 45, 123000000, ZoneId.of("UTC")),
-        ZonedDateTime.of(2025, 1, 21, 21, 14, 45, 120000000, ZoneId.of("UTC")),
-        ZonedDateTime.of(2025, 1, 21, 21, 14, 45, 100000000, ZoneId.of("UTC")),
-        ZonedDateTime.of(2025, 1, 21, 21, 14, 45, 0, ZoneId.of("UTC")),
-        ZonedDateTime.of(2025, 1, 21, 21, 14, 45, 123456000, ZoneId.of("UTC")),
-        ZonedDateTime.ofInstant(Instant.EPOCH, ZoneId.of("UTC")) // Failure Case
-    };
+  String[] inputTimeStrings = {
+      "2025-01-21T21:14:45.123Z",
+      "2025-01-21T21:14:45.12Z",
+      "2025-01-21T21:14:45.1Z",
+      "2025-01-21T21:14:45Z",
+      "2025-01-21T21:14:45.123456Z",
+      "2025/01/21 21:14:45.123456Z" // Failure Case
+  };
+
+  ZonedDateTime[] outputZonedDateTimes = {
+      ZonedDateTime.of(2025, 1, 21, 21, 14, 45, 123000000, ZoneId.of("UTC")),
+      ZonedDateTime.of(2025, 1, 21, 21, 14, 45, 120000000, ZoneId.of("UTC")),
+      ZonedDateTime.of(2025, 1, 21, 21, 14, 45, 100000000, ZoneId.of("UTC")),
+      ZonedDateTime.of(2025, 1, 21, 21, 14, 45, 0, ZoneId.of("UTC")),
+      ZonedDateTime.of(2025, 1, 21, 21, 14, 45, 123456000, ZoneId.of("UTC")),
+      ZonedDateTime.ofInstant(Instant.EPOCH, ZoneId.of("UTC")) // Failure Case
+  };
 
   @Test
   public void testTimestampConversion() {
 
     StringToZonedDateTimeConverter converter = new StringToZonedDateTimeConverter();
 
+    for (int i = 0; i < inputTimeStrings.length; i++) {
 
-    for(int i =0; i< inputTimeStrings.length; i++){
-        System.out.println(inputTimeStrings[i]);
-        ZonedDateTime convertedTime = converter.convert(inputTimeStrings[i]);
+      ZonedDateTime convertedTime = converter.convert(inputTimeStrings[i]);
 
-        
-
-        assertThat(convertedTime.equals(outputZonedDateTimes[i]));
+      // assertThat(convertedTime.equals(outputZonedDateTimes[i]));
+      assertThat(convertedTime).isEqualTo(outputZonedDateTimes[i]);
 
     }
   }
 
 }
-

207431cd41801e7c5a943ec6377d7706e8c80fda
jacob6838 <51723791+jacob6838@users.noreply.github.com>
2025-01-30 16:29:25 -0700

    Update resources/mongo_scripts/a_init_replicas.js
    Co-authored-by: Daniel McCoy Stephenson <dmccoystephenson@gmail.com>

--- resources/mongo_scripts/a_init_replicas.js
+++ resources/mongo_scripts/a_init_replicas.js
@@ -4,7 +4,7 @@
 This script is the first of two scripts responsible for setting up mongoDB on initial boot. 
 These scripts should be copied to the /docker-entrypoint-initdb.d/ directory within the docker image;
 the docker image will then execute these scripts automatically when the database is first created. 
-This script and is partner are prefixed with the letters a and b respectively to ensure they are run 
+This script and its partner are prefixed with the letters a and b respectively to ensure they are run 
 in the proper order when copied into the mongoDB docker image.
 
 Since the conflict monitor uses a replica set in its mongoDB configuration. Initializing the replica set

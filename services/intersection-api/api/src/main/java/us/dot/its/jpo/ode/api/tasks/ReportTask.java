3083eab91b34ac0ca45685c30a2959acbe198a53
jacob6838 <jfrye@neaeraconsulting.com>
2025-01-31 11:57:18 -0700

    Create variables for ReportTask crons

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/tasks/ReportTask.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/tasks/ReportTask.java
@@ -18,13 +18,9 @@ import us.dot.its.jpo.ode.api.models.IntersectionReferenceData;
 import us.dot.its.jpo.ode.api.services.ReportService;
 
 @Component
-@ConditionalOnProperty(
-    name = "enable.report",
-    havingValue = "true",
-    matchIfMissing = false
-)
+@ConditionalOnProperty(name = "enable.report", havingValue = "true", matchIfMissing = false)
 public class ReportTask {
- 
+
     @Autowired
     ReportService reportService;
 
@@ -33,44 +29,48 @@ public class ReportTask {
 
     private static final Logger log = LoggerFactory.getLogger(ReportTask.class);
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    private static final String DAILY_NOTIFICATION_CRON = "0 0 0 * * ?"; // every day at midnight
+    private static final String WEEKLY_NOTIFICATION_CRON = "0 0 0 * * 0"; // every sunday at midnight
+    private static final String MONTHLY_NOTIFICATION_CRON = "0 0 0 1 * ?"; // first day of the month at midnight
 
-    @Scheduled(cron = "0 0 0 * * ?")
-	public void generateDailyReports() {
-		log.info("Generating Daily Report", dateFormat.format(new Date()));
+    @Scheduled(cron = DAILY_NOTIFICATION_CRON)
+    public void generateDailyReports() {
+        log.info("Generating Daily Report", dateFormat.format(new Date()));
         LocalDate today = LocalDate.now(ZoneOffset.UTC);
         LocalTime time = LocalTime.of(0, 0, 0);
         LocalDateTime dateTime = LocalDateTime.of(today, time);
         long endMillis = dateTime.atZone(ZoneOffset.UTC).toInstant().toEpochMilli();
         long startMillis = dateTime.minusDays(1).atZone(ZoneOffset.UTC).toInstant().toEpochMilli();
         generateReportForTimeRange(startMillis, endMillis);
-	}
+    }
 
-    @Scheduled(cron = "0 0 0 * * 0")
-	public void generateWeeklyReports() {
-		log.info("Generating Weekly Report", dateFormat.format(new Date()));
+    @Scheduled(cron = WEEKLY_NOTIFICATION_CRON)
+    public void generateWeeklyReports() {
+        log.info("Generating Weekly Report", dateFormat.format(new Date()));
         LocalDate today = LocalDate.now(ZoneOffset.UTC);
         LocalTime time = LocalTime.of(0, 0, 0);
         LocalDateTime dateTime = LocalDateTime.of(today, time);
         long endMillis = dateTime.atZone(ZoneOffset.UTC).toInstant().toEpochMilli();
         long startMillis = dateTime.minusWeeks(1).atZone(ZoneOffset.UTC).toInstant().toEpochMilli();
         generateReportForTimeRange(startMillis, endMillis);
-	}
+    }
 
-    @Scheduled(cron = "0 0 0 1 * ?")
+    @Scheduled(cron = MONTHLY_NOTIFICATION_CRON)
     public void generateMonthlyReports() {
-		log.info("Generating Monthly Report", dateFormat.format(new Date()));
+        log.info("Generating Monthly Report", dateFormat.format(new Date()));
         LocalDate today = LocalDate.now(ZoneOffset.UTC);
         LocalTime time = LocalTime.of(0, 0, 0);
         LocalDateTime dateTime = LocalDateTime.of(today, time);
         long endMillis = dateTime.atZone(ZoneOffset.UTC).toInstant().toEpochMilli();
         long startMillis = dateTime.minusMonths(1).atZone(ZoneOffset.UTC).toInstant().toEpochMilli();
         generateReportForTimeRange(startMillis, endMillis);
-	}
+    }
 
-    public void generateReportForTimeRange(long startMillis, long endMillis){
-        for(IntersectionReferenceData data: processedMapRepo.getIntersectionIDs()){
-            log.info("Generating Report for Intersection {} Start Time: {} End Time: {}", data.getIntersectionID(), startMillis, endMillis);
-            
+    public void generateReportForTimeRange(long startMillis, long endMillis) {
+        for (IntersectionReferenceData data : processedMapRepo.getIntersectionIDs()) {
+            log.info("Generating Report for Intersection {} Start Time: {} End Time: {}", data.getIntersectionID(),
+                    startMillis, endMillis);
+
             // build report and save it back to the database.
             reportService.buildReport(data.getIntersectionID(), data.getRoadRegulatorID(), startMillis, endMillis);
         }

316c6aac82a9e4887b336f9deda89fcb713f1a74
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2025-02-10 14:30:25 -0700

    Change background color, margin, fontWeight on HorizontalTabs.tsx

--- webapp/src/styles/components/HorizontalTabs.tsx
+++ webapp/src/styles/components/HorizontalTabs.tsx
@@ -4,9 +4,7 @@ import { Link } from 'react-router-dom'
 export const TabListContainer = styled('ol')(({ theme }) => ({
   fontFamily: 'Arial, Helvetica, sans-serif',
   fontWeight: 550,
-  borderBottom: `1px solid ${theme.palette.custom.mapLegendBackground}`,
   paddingLeft: 0,
-  backgroundColor: theme.palette.custom.mapLegendBackground,
 }))
 
 interface TabItemStyledProps {
@@ -16,18 +14,15 @@ interface TabItemStyledProps {
 export const TabItemStyled = styled(Link)<TabItemStyledProps>(({ theme, isActive }) => ({
   display: 'inline-block',
   listStyle: 'none',
-  marginBottom: '-1px',
   padding: '0.5rem 0.75rem',
   cursor: 'pointer',
   color: theme.palette.text.primary,
   textDecoration: 'none',
   ...(isActive && {
     fontFamily: 'Arial, Helvetica, sans-serif',
-    fontWeight: 550,
+    fontWeight: 750,
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.primary.contrastText,
-    border: `solid ${theme.palette.primary.main}`,
-    borderWidth: '1px 1px 0 1px',
-    borderTop: `0.5px solid ${theme.palette.secondary.dark}`,
+    borderBottom: '2px solid white',
   }),
 }))

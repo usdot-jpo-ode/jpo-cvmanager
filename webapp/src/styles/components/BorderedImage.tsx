b16591f2fc3216701689505ece49f58a94b92347
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2025-04-04 12:59:21 -0600

    Remove width attribute from BorderedImage

--- webapp/src/styles/components/BorderedImage.tsx
+++ webapp/src/styles/components/BorderedImage.tsx
@@ -4,7 +4,6 @@ export const BorderedImage = styled('img')(({ theme }) => ({
   display: 'block',
   marginLeft: 'auto',
   marginRight: 'auto',
-  width: '40%',
   marginTop: '4em',
   marginBottom: '4em',
   border: `0.5px solid ${theme.palette.text.primary}}`,

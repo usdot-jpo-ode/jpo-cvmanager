8cf63548cbf36908b81de6894263807d30911d60
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2025-04-14 07:34:30 -0600

    Remove margin, border from BorderedImage.tsx

--- webapp/src/styles/components/BorderedImage.tsx
+++ webapp/src/styles/components/BorderedImage.tsx
@@ -4,7 +4,4 @@ export const BorderedImage = styled('img')(({ theme }) => ({
   display: 'block',
   marginLeft: 'auto',
   marginRight: 'auto',
-  marginTop: '4em',
-  marginBottom: '4em',
-  border: `0.5px solid ${theme.palette.text.primary}}`,
 }))

58563f2c6eb7c41190b5e9dc6bc12fd069230e11
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2025-04-18 11:21:07 -0600

    Update layout to use stack, update Destination IP input to have fixed label

--- webapp/src/components/SnmpsetMenu.tsx
+++ webapp/src/components/SnmpsetMenu.tsx
@@ -19,7 +19,7 @@ import { RootState } from '../store'
 
 import './css/SnmpwalkMenu.css'
 import toast from 'react-hot-toast'
-import { Box, Button, FormControl, Grid2, InputLabel, MenuItem, OutlinedInput, Select } from '@mui/material'
+import { Button, FormControl, Grid2, InputLabel, MenuItem, Select, Stack, TextField, Typography } from '@mui/material'
 import { ControlPointOutlined, DeleteOutline } from '@mui/icons-material'
 
 export type SnmpsetMenuProps = {
@@ -41,22 +41,28 @@ const SnmpsetMenu = (props: SnmpsetMenuProps) => {
     <div>
       <form id="snmpform">
         <Grid2 container spacing={2}>
-          <Grid2 size={6}>
+          <Grid2 size={8}>
             <FormControl fullWidth margin="normal">
-              <InputLabel htmlFor="snmpinput">Destination IP</InputLabel>
-              <OutlinedInput
+              <TextField
                 label="Destination IP"
+                placeholder="Enter Destination IP"
                 id="snmpinput"
                 color="info"
+                variant="outlined"
                 value={destIp}
                 onChange={(event) => {
                   var ip = event.target.value as string
                   dispatch(setDestIp(ip))
                 }}
+                slotProps={{
+                  inputLabel: {
+                    shrink: true,
+                  },
+                }}
               />
             </FormControl>
           </Grid2>
-          <Grid2 size={6}>
+          <Grid2 size={4}>
             <FormControl fullWidth margin="normal">
               <InputLabel htmlFor="msg-type-select">Message Type</InputLabel>
               <Select
@@ -78,93 +84,92 @@ const SnmpsetMenu = (props: SnmpsetMenuProps) => {
           </Grid2>
         </Grid2>
       </form>
-
-      <Button
-        className="museo-slab"
-        variant="contained"
-        size="medium"
-        startIcon={<ControlPointOutlined />}
-        onClick={() =>
-          dispatch(submitSnmpSet(rsuIpList)).then((data: any) => {
-            data.payload.changeSuccess
-              ? toast.success('Forwarding Changes Applied Successfully')
-              : toast.error('Failed to add forwarding: ', data.errorState)
-          })
-        }
-      >
-        Add Forwarding
-      </Button>
-      <Box />
-      {type !== 'single_rsu' && (
-        <Button
-          className="museo-slab"
-          variant="contained"
-          size="medium"
-          startIcon={<DeleteOutline />}
-          sx={{
-            marginTop: '10px',
-          }}
-          onClick={() =>
-            dispatch(
-              deleteSnmpSet({
-                ipList: rsuIpList,
-                snmpMsgType: snmpMsgType,
-                destIp: destIp,
-              })
-            )
-          }
-        >
-          Delete Forwarding
-        </Button>
-      )}
-
-      {type !== 'single_rsu' ? (
+      <Stack spacing={2} direction="column">
         <div>
-          <p id="snmpfiltertext" style={{ marginTop: '40px' }}>
-            By specifying a destination IP address along with a message type, you can add message forwarding to any of
-            the selected RSUs or delete message forwarding from any RSUs that already have that configuration.
-          </p>
-        </div>
-      ) : (
-        <div />
-      )}
-
-      {rsuManufacturer === 'Yunex' ? (
-        <div>
-          <p id="snmpfiltertext" style={{ marginTop: '40px' }}>
-            Yunex RSUs use different SNMP tables for message TX and RX forwarding. <br /> BSM and SSM are on the RX
-            table. MAP, SPaT and SRM are on the TX table. <br /> Start over from the 1 index for each table.
-          </p>
-        </div>
-      ) : (
-        <div />
-      )}
-
-      {rsuManufacturer === 'Commsignia' ? (
-        <div>
-          <p id="snmpfiltertext" style={{ marginTop: '40px' }}>
-            If you are configuring SPaT or MAP forwarding, apply the TX message <br /> filter after your configuration
-            has been applied
-          </p>
           <Button
             className="museo-slab"
             variant="contained"
             size="medium"
             startIcon={<ControlPointOutlined />}
             onClick={() =>
-              dispatch(filterSnmp([rsuIp])).then((data: any) => {
-                data.snmpFilterErr
-                  ? toast.error('Failed to apply TX filter: ', data.snmpFilterMsg)
-                  : toast.success('TX Filter Applied Successfully')
+              dispatch(submitSnmpSet(rsuIpList)).then((data: any) => {
+                data.payload.changeSuccess
+                  ? toast.success('Forwarding Changes Applied Successfully')
+                  : toast.error('Failed to add forwarding: ', data.errorState)
               })
             }
           >
-            Apply TX Filter
+            Add Forwarding
           </Button>
         </div>
-      ) : (
-        <div />
-      )}
+        {type !== 'single_rsu' && (
+          <Button
+            className="museo-slab"
+            variant="contained"
+            size="medium"
+            startIcon={<DeleteOutline />}
+            sx={{
+              marginTop: '10px',
+            }}
+            onClick={() =>
+              dispatch(
+                deleteSnmpSet({
+                  ipList: rsuIpList,
+                  snmpMsgType: snmpMsgType,
+                  destIp: destIp,
+                })
+              )
+            }
+          >
+            Delete Forwarding
+          </Button>
+        )}
+
+        {type !== 'single_rsu' ? (
+          <div>
+            <Typography variant="body1">
+              By specifying a destination IP address along with a message type, you can add message forwarding to any of
+              the selected RSUs or delete message forwarding from any RSUs that already have that configuration.
+            </Typography>
+          </div>
+        ) : null}
+
+        {rsuManufacturer === 'Yunex' ? (
+          <div>
+            <Typography variant="body1">
+              Yunex RSUs use different SNMP tables for message TX and RX forwarding. <br /> BSM and SSM are on the RX
+              table. MAP, SPaT and SRM are on the TX table. <br /> Start over from the 1 index for each table.
+            </Typography>
+          </div>
+        ) : null}
+
+        {rsuManufacturer === 'Commsignia' ? (
+          <div>
+            <Typography variant="body1">
+              If you are configuring SPaT or MAP forwarding, apply the TX message filter after your configuration has
+              been applied
+            </Typography>
+            <Button
+              className="museo-slab"
+              variant="contained"
+              size="medium"
+              startIcon={<ControlPointOutlined />}
+              sx={{
+                mt: 2,
+              }}
+              onClick={() =>
+                dispatch(filterSnmp([rsuIp])).then((data: any) => {
+                  data.snmpFilterErr
+                    ? toast.error('Failed to apply TX filter: ', data.snmpFilterMsg)
+                    : toast.success('TX Filter Applied Successfully')
+                })
+              }
+            >
+              Apply TX Filter
+            </Button>
+          </div>
+        ) : null}
+      </Stack>
     </div>
   )
 }

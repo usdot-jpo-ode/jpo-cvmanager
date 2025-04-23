3e972ad17433c1169142c534257cc0b634da898e
jacob6838 <51723791+jacob6838@users.noreply.github.com>
2025-04-23 17:11:46 -0600

    Update services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/accessors/IntersectionCriteria.java
    Co-authored-by: Matt Cook <mattheworion.cook@gmail.com>

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/accessors/IntersectionCriteria.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/accessors/IntersectionCriteria.java
@@ -26,16 +26,15 @@ public class IntersectionCriteria extends Criteria {
             @Nullable boolean formatAsString) {
         if (startEpochMillis != null && endEpochMillis != null) {
             this.and(fieldName)
-                    .gte(formatAsString == true ? Instant.ofEpochMilli(startEpochMillis).toString()
+                    .gte(formatAsString ? Instant.ofEpochMilli(startEpochMillis).toString()
                             : Date.from(Instant.ofEpochMilli(startEpochMillis)))
-                    .lte(formatAsString == true ? Instant.ofEpochMilli(endEpochMillis).toString()
+                    .lte(formatAsString ? Instant.ofEpochMilli(endEpochMillis).toString()
                             : Date.from(Instant.ofEpochMilli(endEpochMillis)));
-            return this;
         } else if (startEpochMillis != null) {
-            this.and(fieldName).gte(formatAsString == true ? Instant.ofEpochMilli(startEpochMillis).toString()
+            this.and(fieldName).gte(formatAsString ? Instant.ofEpochMilli(startEpochMillis).toString()
                     : Date.from(Instant.ofEpochMilli(startEpochMillis)));
         } else if (endEpochMillis != null) {
-            this.and(fieldName).lte(formatAsString == true ? Instant.ofEpochMilli(endEpochMillis).toString()
+            this.and(fieldName).lte(formatAsString ? Instant.ofEpochMilli(endEpochMillis).toString()
                     : Date.from(Instant.ofEpochMilli(endEpochMillis)));
         }
         return this;

b751d0ae92d466fd6afcc4e0ad43fb2b229c2eb9
jacob6838 <jfrye@neaeraconsulting.com>
2025-04-29 06:08:07 -0600

    Re-adding roadRegulatorId to webapp cm types

--- webapp/src/models/jpo-conflictmonitor/assessments/Assessment.d.ts
+++ webapp/src/models/jpo-conflictmonitor/assessments/Assessment.d.ts
@@ -2,5 +2,6 @@ type Assessment = {
   assessmentGeneratedAt: number
   assessmentType: string
   intersectionID: number
+  roadRegulatorID: number // This field is being phased out, but is left in because this field is still supplied by the ConflictMonitor
   source: string
 }
--- webapp/src/models/jpo-conflictmonitor/notifications/Notification.d.ts
+++ webapp/src/models/jpo-conflictmonitor/notifications/Notification.d.ts
@@ -8,6 +8,7 @@ declare namespace MessageMonitor {
     notificationText: string
     notificationHeading: string
     intersectionID: number
+    roadRegulatorID: number // This field is being phased out, but is left in because this field is still supplied by the ConflictMonitor
     notificationExpiresAt: Date
     getUniqueId: string
   }

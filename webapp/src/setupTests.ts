0716d9886124055a0d5e236ba9cb91e910646f6d
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2025-06-24 11:40:57 -0600

    Fix enableMocks call in setupTests.ts

--- webapp/src/setupTests.ts
+++ webapp/src/setupTests.ts
@@ -1,8 +1,8 @@
 import 'jest-canvas-mock'
 
 // adds the 'fetchMock' global variable and rewires 'fetch' global to call 'fetchMock' instead of the real implementation
-import enableMocks from 'jest-fetch-mock'
-enableMocks()
+import fetchMock from 'jest-fetch-mock'
+fetchMock.enableMocks()
 // changes default behavior of fetchMock to use the real 'fetch' implementation and not mock responses
 fetchMock.dontMock()
 // browser mocks

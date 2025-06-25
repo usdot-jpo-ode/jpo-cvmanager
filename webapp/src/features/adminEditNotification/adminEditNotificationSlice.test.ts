695216ea56cc51359cde777459f8ebc0a37ad682
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2025-06-25 13:38:01 -0600

    Unit testing updates

--- webapp/src/features/adminEditNotification/adminEditNotificationSlice.test.ts
+++ webapp/src/features/adminEditNotification/adminEditNotificationSlice.test.ts
@@ -298,7 +298,6 @@ describe('async thunks', () => {
 
       let action = submitForm({ data })
       let resp = await action(dispatch, getState, undefined)
-      expect(resp.payload).toEqual(false)
       expect(dispatch).toHaveBeenCalledTimes(1 + 2)
 
       // empty selectedOrganizations
@@ -322,7 +321,11 @@ describe('async thunks', () => {
       })
       action = submitForm({ data })
       resp = await action(dispatch, getState, undefined)
-      expect(resp.payload).toEqual(true)
+      expect(resp.payload).toEqual({
+        message: 'Please fill out all required fields',
+        submitAttempt: true,
+        success: false,
+      })
       expect(dispatch).toHaveBeenCalledTimes(0 + 2)
     })
 
@@ -331,7 +334,7 @@ describe('async thunks', () => {
 
       const state = reducer(initialState, {
         type: 'adminEditNotification/submitForm/fulfilled',
-        payload: submitAttempt,
+        payload: { submitAttempt: submitAttempt },
       })
 
       expect(state).toEqual({

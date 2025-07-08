9e295bd217a49ba5ad3c5250290cf6d4ebd949f6
jacob6838 <jfrye@neaeraconsulting.com>
2025-07-08 16:58:52 -0600

    Updating IDCount lombok config

--- services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/models/IDCount.java
+++ services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/models/IDCount.java
@@ -1,18 +1,12 @@
 package us.dot.its.jpo.ode.api.models;
 
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
+import lombok.Data;
 
-@ToString
-@Setter
-@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
+@Data
 public class IDCount {
     private String id;
     private double count;

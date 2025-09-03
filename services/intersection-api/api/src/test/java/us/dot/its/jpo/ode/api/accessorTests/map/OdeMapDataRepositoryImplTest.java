1370207fa6fdf509f5760691d94e37a29bef8198
john-wiens <jwiens3141@gmail.com>
2025-09-03 16:45:02 -0600

    Updating Repository Impl Tests to new Data structures

--- services/intersection-api/api/src/test/java/us/dot/its/jpo/ode/api/accessorTests/map/OdeMapDataRepositoryImplTest.java
+++ services/intersection-api/api/src/test/java/us/dot/its/jpo/ode/api/accessorTests/map/OdeMapDataRepositoryImplTest.java
@@ -25,13 +25,13 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import us.dot.its.jpo.asn.j2735.r2024.MapData.MapDataMessageFrame;
 import us.dot.its.jpo.ode.api.accessors.map.OdeMapDataRepositoryImpl;
 import us.dot.its.jpo.ode.api.models.AggregationResult;
-import us.dot.its.jpo.ode.model.OdeMapData;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
@@ -42,7 +42,7 @@ import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 @AutoConfigureEmbeddedDatabase
 public class OdeMapDataRepositoryImplTest {
 
-    @SpyBean
+    @MockitoSpyBean
     private MongoTemplate mongoTemplate;
 
     @Mock
@@ -52,7 +52,7 @@ public class OdeMapDataRepositoryImplTest {
     private Page<Document> mockDocumentPage;
 
     @Mock
-    private Page<OdeMapData> mockPage;
+    private Page<MapDataMessageFrame> mockPage;
 
     @InjectMocks
     private OdeMapDataRepositoryImpl repository;
@@ -93,11 +93,11 @@ public class OdeMapDataRepositoryImplTest {
                 any(Criteria.class),
                 any(Sort.class),
                 any(),
-                eq(OdeMapData.class))).thenReturn(mockPage);
+                eq(MapDataMessageFrame.class))).thenReturn(mockPage);
         PageRequest pageRequest = PageRequest.of(0, 1);
         doCallRealMethod().when(repo).find(1, null, null, pageRequest);
 
-        Page<OdeMapData> results = repo.find(1, null, null, pageRequest);
+        Page<MapDataMessageFrame> results = repo.find(1, null, null, pageRequest);
 
         assertThat(results).isEqualTo(mockPage);
     }

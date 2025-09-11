d0e2c39359f30d2c1c61ff3cbe354351d9a515d1
jacob6838 <jfrye@neaeraconsulting.com>
2025-09-10 22:12:58 -0800

    Updating processed BSM, SPAT, and MAP webapp types

--- webapp/src/models/convert_java_to_ts.py
+++ webapp/src/models/convert_java_to_ts.py
@@ -7,25 +7,20 @@ matching_expr_interface = r"public interface (.*?)\{"
 
 TYPE_MAPPINGS = {
     # Simple types
-    '(^String)': 'str',
-
-    '(^int)': 'number',
-    '(^Integer)': 'number',
-    '(^Double)': 'number',
-    '(^double)': 'number',
-    '(^long)': 'number',
-    '(^Long)': 'number',
-    'final': 'any',
-
-    '(^Boolean)': 'boolean',
-    '(^bool)': 'boolean',
-
-    '^ArrayList<(.*?)>': r'\1[]',
-    '^List<(.*?)>': r'\1[]',
-
-    '(^LineString)': 'number[][]',
-    '(^ZonedDateTime)': 'Date',
-
+    "(^String)": "str",
+    "(^int)": "number",
+    "(^Integer)": "number",
+    "(^Double)": "number",
+    "(^double)": "number",
+    "(^long)": "number",
+    "(^Long)": "number",
+    "final": "any",
+    "(^Boolean)": "boolean",
+    "(^bool)": "boolean",
+    "^ArrayList<(.*?)>": r"\1[]",
+    "^List<(.*?)>": r"\1[]",
+    "(^LineString)": "number[][]",
+    "(^ZonedDateTime)": "Date",
     # '(Set<(.*?)>)': r'Set<\1>',
 }
 
@@ -39,12 +34,26 @@ type {class_name} = {extension}{{
 #     os.mkdir(IMPORTS_PATH)
 
 # INITIAL_PATH = "java/jpo-conflictmonitor/jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/models"
-INITIAL_PATH = "../../../api/jpo-conflictmonitor/jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/models"
-files = [os.path.join(dp, f) for dp, dn, filenames in os.walk("./" + INITIAL_PATH)
-         for f in filenames if os.path.splitext(f)[1] == '.java']
+INITIAL_PATH = (
+    "../../../services/intersection-api/api/src/main/java/us/dot/its/jpo/ode/api/models"
+)
+files = [
+    os.path.join(dp, f)
+    for dp, dn, filenames in os.walk("./" + INITIAL_PATH)
+    for f in filenames
+    if os.path.splitext(f)[1] == ".java"
+]
 
-DIRECTORIES_TO_IGNORE = ["test", "jpo-s3-deposit", "jpo-ode-consumer-example",
-                         "jpo-ode-svcs", "jpo-sdw-depositor", "jpo-security-scvs", "asn1_codec", "jpo-geojsonconverter"]
+DIRECTORIES_TO_IGNORE = [
+    "test",
+    "jpo-s3-deposit",
+    "jpo-ode-consumer-example",
+    "jpo-ode-svcs",
+    "jpo-sdw-depositor",
+    "jpo-security-scvs",
+    "asn1_codec",
+    "jpo-geojsonconverter",
+]
 
 CUSTOM_DIRECTORIES_TO_NOT_REGENERATE = ["config"]
 
@@ -52,13 +61,13 @@ for file_path in files:
     print(file_path)
     imports = ""
 
-    file_path = '.'.join(file_path.replace('\\', '/').split('.')[:-1])
-    JAVA_PATH = '/'.join(file_path.split('/')[:-1])
-    TS_PATH = JAVA_PATH.replace(INITIAL_PATH, 'jpo-conflictmonitor/')
-    CLASS_NAME = file_path.split('/')[-1]
+    file_path = ".".join(file_path.replace("\\", "/").split(".")[:-1])
+    JAVA_PATH = "/".join(file_path.split("/")[:-1])
+    TS_PATH = JAVA_PATH.replace(INITIAL_PATH, "jpo-conflictmonitor/")
+    CLASS_NAME = file_path.split("/")[-1]
 
     ignored = False
-    directories = file_path.split('/')
+    directories = file_path.split("/")
     for ignored_dir in DIRECTORIES_TO_IGNORE:
         if ignored_dir in directories:
             ignored = True
@@ -68,7 +77,7 @@ for file_path in files:
     if ignored:
         continue
 
-    file_contents = open(f"{JAVA_PATH}/{CLASS_NAME}.java", 'r').read()
+    file_contents = open(f"{JAVA_PATH}/{CLASS_NAME}.java", "r").read()
     interface_match = re.findall(matching_expr_interface, file_contents)
     if interface_match:
         continue
@@ -78,19 +87,24 @@ for file_path in files:
     if extension_match and extension_match[0][1]:
         if extension_match[0][1].strip() == "Notification":
             imports = '/// <reference path="Notification.d.ts" />'
-            extension = f"MessageMonitor.{extension_match[0][1]} & " if extension_match else ""
+            extension = (
+                f"MessageMonitor.{extension_match[0][1]} & " if extension_match else ""
+            )
         elif extension_match[0][1].strip() == "Event":
             imports = '/// <reference path="Event.d.ts" />'
-            extension = f"MessageMonitor.{extension_match[0][1]} & " if extension_match else ""
+            extension = (
+                f"MessageMonitor.{extension_match[0][1]} & " if extension_match else ""
+            )
         else:
             extension = f"{extension_match[0][1]} & " if extension_match else ""
-            
+
     matches = []
-    lines = file_contents.split('\n')
+    lines = file_contents.split("\n")
     for l in lines:
         match = re.search(matching_expr, l)
-        if match: matches.append(match.groups())
-        
+        if match:
+            matches.append(match.groups())
+
     print(matches)
     lines = []
     for var in matches:
@@ -100,7 +114,7 @@ for file_path in files:
             ts_type = re.sub(k, v, ts_type)
         lines.append(f"  {ts_name}: {ts_type}")
 
-    contents = '\n'.join(lines)
+    contents = "\n".join(lines)
     if CLASS_NAME == "Notification":
         imports = "declare namespace MessageMonitor {"
         contents += "\n}"
@@ -109,9 +123,10 @@ for file_path in files:
         contents += "\n}"
 
     ts_contents = TEMPLATE.format(
-        imports=imports, class_name=CLASS_NAME, extension=extension, contents=contents)
+        imports=imports, class_name=CLASS_NAME, extension=extension, contents=contents
+    )
 
     if not os.path.exists(f"{TS_PATH}/"):
         os.makedirs(f"{TS_PATH}/")
-    with open(f"{TS_PATH}/{CLASS_NAME}.d.ts", 'w') as f:
+    with open(f"{TS_PATH}/{CLASS_NAME}.d.ts", "w") as f:
         f.write(ts_contents)
--- webapp/src/models/imports/ProcessedMap.d.ts
+++ webapp/src/models/imports/ProcessedMap.d.ts
@@ -5,7 +5,8 @@ type ProcessedMap = {
 }
 
 type MapSharedProperties = {
-  messageType: string
+  schemaVersion: number
+  messageType: 'MAP'
   odeReceivedAt: number
   originIp: string
   intersectionName?: string
@@ -45,6 +46,7 @@ type MapProperties = {
   nodes: MapNode[]
   laneId: number
   laneName?: string
+  laneType?: object
   sharedWith: J2735LaneSharing
   egressApproach: number
   ingressApproach: number
@@ -91,4 +93,134 @@ type ConnectingLanesProperties = {
   signalGroupId: number | null
   ingressLaneId: number
   egressLaneId: number
+}
+
+type LaneTypeAttributes = {
+  vehicle?: LaneAttributes_Vehicle
+  crosswalk?: LaneAttributes_Crosswalk
+  bikeLane?: LaneAttributes_Bike
+  sidewalk?: LaneAttributes_Sidewalk
+  median?: LaneAttributes_Barrier
+  striping?: LaneAttributes_Striping
+  trackedVehicle?: LaneAttributes_TrackedVehicle
+  parking?: LaneAttributes_Parking
+}
+
+type LaneAttributes_Vehicle = {
+  isVehicleRevocableLane: boolean // this lane may be activated or not based on the current SPAT message contents if not asserted, the lane is ALWAYS present
+  isVehicleFlyOverLane: boolean // path of lane is not at grade
+  hovLaneUseOnly: boolean
+  restrictedToBusUse: boolean
+  restrictedToTaxiUse: boolean
+  restrictedFromPublicUse: boolean
+  hasIRbeaconCoverage: boolean
+  permissionOnRequest: boolean // to inform about a lane for e-cars
+}
+
+type LaneAttributes_Crosswalk = {
+  crosswalkRevocableLane: boolean // this lane may be activated or not based
+  // on the current SPAT message contents
+  // if not asserted, the lane is ALWAYS present
+  bicyleUseAllowed: boolean // The path allows bicycle traffic,
+  // if not set, this mode is prohibited
+  isXwalkFlyOverLane: boolean // path of lane is not at grade
+  fixedCycleTime: boolean // ped walk phases use preset times
+  // i.e., there is not a ‘push to cross’ button
+  biDirectionalCycleTimes: boolean // ped walk phases use different SignalGroupID
+  // for each direction. The first SignalGroupID
+  // in the first Connection represents ‘inbound’
+  // flow (the direction of travel towards the first
+  // node point) while second SignalGroupID in the
+  // next Connection entry represents the ‘outbound’
+  // flow. And use of RestrictionClassID entries
+  // in the Connect follow this same pattern in pairs.
+  hasPushToWalkButton: boolean // Has a demand input
+  audioSupport: boolean // audio crossing cues present
+  rfSignalRequestPresent: boolean // Supports RF push to walk technologies
+  unsignalizedSegmentsPresent: boolean // The lane path consists of one of more segments that are not signal controlled
+}
+
+type LaneAttributes_Bike = {
+  bikeRevocableLane: boolean // this lane may be activated or not based
+  // based on the current SPAT message contents
+  // if not asserted, the lane is ALWAYS present
+  pedestrianUseAllowed: boolean // The path allows pedestrian traffic,
+  // if not set, this mode is prohibited
+  isBikeFlyOverLane: boolean // path of lane is not at grade
+  fixedCycleTime: boolean // the phases use preset times
+  // i.e., there is not a ‘push to cross’ button
+  biDirectionalCycleTimes: boolean // ped walk phases use different SignalGroupID
+  // for each direction. The first SignalGroupID
+  // in the first Connection represents ‘inbound’
+  // flow (the direction of travel towards the first
+  // node point) while second SignalGroupID in the
+  // next Connection entry represents the ‘outbound’
+  // flow. And use of RestrictionClassID entries
+}
+
+type LaneAttributes_Sidewalk = {
+  sidewalkRevocableLane: boolean // this lane may be activated or not based
+  // on the current SPAT message contents
+  // if not asserted, the lane is ALWAYS present
+  bicyleUseAllowed: boolean // The path allows bicycle traffic,
+  // if not set, this mode is prohibited
+  isSidewalkFlyOverLane: boolean // path of lane is not at grade
+  walkBikes: boolean // bike traffic must dismount and walk
+}
+
+type LaneAttributes_Barrier = {
+  medianRevocableLane: boolean // this lane may be activated or not based
+  // based on the current SPAT message contents
+  // if not asserted, the lane is ALWAYS present
+  median: boolean
+  whiteLineHashing: boolean
+  stripedLines: boolean
+  doubleStripedLines: boolean
+  trafficCones: boolean
+  constructionBarrier: boolean
+  trafficChannels: boolean
+  lowCurbs: boolean
+  highCurbs: boolean
+}
+
+type LaneAttributes_Striping = {
+  stripeToConnectingLanesRevocableLane: boolean // this lane may be activated or not activated based
+  // on the current SPAT message contents
+  // if not asserted, the lane is ALWAYS present
+  stripeDrawOnLeft: boolean
+  stripeDrawOnRight: boolean // which side of lane to mark
+  stripeToConnectingLanesLeft: boolean
+  stripeToConnectingLanesRight: boolean
+  stripeToConnectingLanesAhead: boolean
+  // the stripe type should be
+  // presented to the user visually
+  // to reflect stripes in the
+  // intersection for the type of
+  // movement indicated
+}
+
+type LaneAttributes_TrackedVehicle = {
+  specRevicableLane: boolean // this lane may be activated or not based
+  // on the current SPAT message contents
+  // if not asserted, the lane is ALWAYS present
+  specCommuterRailRoadTrack: boolean
+  specLightRailRoadTrack: boolean
+  specHeavyRailRoadTrack: boolean
+  specOtherRailType: boolean
+}
+
+type LaneAttributes_Parking = {
+  parkingRevocableLane: boolean // this lane may be activated or not based
+  // on the current SPAT message contents
+  // if not asserted, the lane is ALWAYS present
+  parallelParkingInUse: boolean
+  headInParkingInUse: boolean
+  doNotParkZone: boolean
+  // used to denote fire hydrants as well as
+  // short disruptions in a parking zone
+  parkingForBusUse: boolean
+  parkingForTaxiUse: boolean
+  noPublicParkingUse: boolean
+  // private parking, as in front of
+  // private property
 }

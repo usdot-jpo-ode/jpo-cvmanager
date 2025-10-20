14d4950229ee08bd27d1567976172b43d16a824a
jacob6838 <jfrye@neaeraconsulting.com>
2025-10-20 17:37:37 -0600

    Update timeSyncSlice.ts

--- webapp/src/generalSlices/timeSyncSlice.ts
+++ webapp/src/generalSlices/timeSyncSlice.ts
@@ -20,18 +20,18 @@ export const computeAccurateTimeMillis = (utcMillis: number, timeOffsetMillis: n
 export const getAccurateTimeMillis = (timeOffsetMillis: number): number => Date.now() + timeOffsetMillis
 
 export const syncTimeOffset = createAsyncThunk('timeSync/syncTimeOffset', async (_) => {
-  const start = Date.now() // Record the start time
-  const response = await fetch(TIME_SERVER_URL_UTC)
-  const end = Date.now() // Record the end time
+    const start = Date.now() // Record the start time
+    const response = await fetch(TIME_SERVER_URL_UTC)
+    const end = Date.now() // Record the end time
 
-  const rtt = end - start // Calculate round-trip time
-  const data = await response.json()
-  const serverTime = new Date(data.dateTime).getTime()
+    const rtt = end - start // Calculate round-trip time
+    const data = await response.json()
+    const serverTime = new Date(data.dateTime + 'Z').getTime()
 
-  // Adjust for half the round-trip time
-  const correctedTime = serverTime + rtt / 2
-  const currentTime = Date.now()
-  return correctedTime - currentTime
+    // Adjust for half the round-trip time
+    const correctedTime = serverTime + rtt / 2
+    const currentTime = Date.now()
+    return correctedTime - currentTime
 })
 
 const timeSyncSlice = createSlice({

c0f67625f0c1df60692c14a07499ca75c742ae6c
jacob6838 <jfrye@neaeraconsulting.com>
2026-07-28 12:34:23 +1200

    Merging ssm/srm decoder tests

--- services/intersection-api/api/src/test/java/us/dot/its/jpo/ode/api/asn1/SrmDecoderTests.java
+++ services/intersection-api/api/src/test/java/us/dot/its/jpo/ode/api/asn1/SrmDecoderTests.java
@@ -3,6 +3,7 @@ package us.dot.its.jpo.ode.api.asn1;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.ZonedDateTime;
 
 import j2735ffm.MessageFrameCodec;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,18 +13,25 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import us.dot.its.jpo.geojsonconverter.DateJsonMapper;
+import us.dot.its.jpo.geojsonconverter.pojos.geojson.srm.ProcessedSrm;
 import us.dot.its.jpo.ode.model.OdeMessageFrameData;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 public class SrmDecoderTests {
 
     private SrmDecoder srmDecoder;
+    private String odeSrmDecodedXmlReference = "";
+    private String odeSrmDecodedJsonReference = "";
+    private String processedSrmReference = "";
+    private String odeReceivedAt = "2025-08-29T16:09:34.416Z";
 
-    private String odeSrmDecodedXmlReference;
-    private String odeSrmDecodedJsonReference;
+    ObjectMapper objectMapper;
 
     @Mock
     MessageFrameCodec messageFrameCodec;
@@ -32,25 +40,66 @@ public class SrmDecoderTests {
     void setUp() throws IOException {
         srmDecoder = new SrmDecoder(messageFrameCodec);
 
-        odeSrmDecodedXmlReference = new String(
-                Files.readAllBytes(Paths.get("src/test/resources/xml/Ode.ReferenceSrmXER.xml")));
+        objectMapper = DateJsonMapper.getInstance();
 
-        odeSrmDecodedJsonReference = new String(
-                Files.readAllBytes(Paths.get("src/test/resources/json/srm/Ode.ReferenceSrmJson.json")))
-                .replaceAll("\n", "").replaceAll(" ", "");
+        try {
+            odeSrmDecodedXmlReference = new String(
+                    Files.readAllBytes(Paths.get("src/test/resources/xml/Ode.ReferenceSrmXER.xml")));
+
+            odeSrmDecodedJsonReference = new String(
+                    Files.readAllBytes(Paths
+                            .get("src/test/resources/json/srm/Ode.ReferenceSrmJson.json")))
+                    .replaceAll("\n", "").replaceAll(" ", "");
+
+            processedSrmReference = new String(
+                    Files.readAllBytes(Paths
+                            .get("src/test/resources/json/srm/GJC.ReferenceProcessedSrmJson.json")))
+                    .replaceAll("\n", "").replaceAll(" ", "");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
-     * Test verifying the conversion from String XML data to OdeMessageFrame Object
+     * Test verifying the conversion from String XML data to OdeMessageFrame
+     * Object
      */
     @Test
-    public void testGetAsMessageFrame() throws JsonProcessingException {
-        OdeMessageFrameData srm = srmDecoder.convertXERToMessageFrame(odeSrmDecodedXmlReference);
+    public void testGetAsMessageFrame() {
+        try {
+            OdeMessageFrameData srm = srmDecoder.convertXERToMessageFrame(odeSrmDecodedXmlReference);
 
-        srm.getMetadata().setOdeReceivedAt("2025-08-29T16:09:34.416Z");
-        srm.getMetadata()
-                .setSerialId(srm.getMetadata().getSerialId().setStreamId("44a6d71c-8af1-4f45-848c-10bd7f919be8"));
+            srm.getMetadata().setOdeReceivedAt(odeReceivedAt);
+            srm.getMetadata()
+                    .setSerialId(srm.getMetadata().getSerialId().setStreamId("44a6d71c-8af1-4f45-848c-10bd7f919be8"));
 
-        assertThatJson(odeSrmDecodedJsonReference).isEqualTo(srm.toJson());
+            assertThatJson(odeSrmDecodedJsonReference).isEqualTo(srm.toJson());
+        } catch (JsonProcessingException e) {
+            assertEquals(true, false);
+        }
+    }
+
+    /**
+     * Test to verify Conversion from a OdeMessageFrame object to a ProcessedSrm
+     * Object
+     */
+    @Test
+    public void testConvertMessageFrameToProcessedSrm() {
+
+        try {
+            OdeMessageFrameData srmMessageFrame = objectMapper.readValue(odeSrmDecodedJsonReference,
+                    OdeMessageFrameData.class);
+
+            srmMessageFrame.getMetadata().setOdeReceivedAt(odeReceivedAt);
+
+            ProcessedSrm srm = srmDecoder.convertMessageFrameToProcessedSrm(srmMessageFrame);
+
+            srm.getProperties().setOdeReceivedAt(ZonedDateTime.parse(odeReceivedAt));
+
+            assertEquals(srm.toString().replaceAll("\n", "").replaceAll(" ", ""), processedSrmReference);
+        } catch (JsonProcessingException e) {
+            assertEquals(true, false);
+        }
     }
 }

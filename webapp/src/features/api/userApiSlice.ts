132fcf4628e35c58b48339894da9169607161e75
jacob6838 <jfrye@neaeraconsulting.com>
2026-07-29 07:30:23 +1200

    Resolving sonarcloud issues

--- webapp/src/features/api/userApiSlice.ts
+++ webapp/src/features/api/userApiSlice.ts
@@ -23,10 +23,7 @@ export const userApiSlice = createApi({
       const token = selectToken(currentState)
 
       headers.set('Accept', 'application/json')
-
-      // Endpoint names must match the keys in the endpoints objects below
-      const endpointsWithoutToken = []
-      if (token && !endpointsWithoutToken.includes(endpoint)) {
+      if (token) {
         headers.set('Authorization', `Bearer ${token}`)
       }
 

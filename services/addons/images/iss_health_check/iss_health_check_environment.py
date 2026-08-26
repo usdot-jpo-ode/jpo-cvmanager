44a0609da2dece00ac6ad2b9c51612ec67d804d6
jacob6838 <jfrye@neaeraconsulting.com>
2026-08-26 14:38:15 +1200

    Updating ISS health check env var requirements for optional vars

--- services/addons/images/iss_health_check/iss_health_check_environment.py
+++ services/addons/images/iss_health_check/iss_health_check_environment.py
@@ -11,9 +11,9 @@ def process_storage_type(val: str, default: str) -> str:
 
 
 PROJECT_ID=get_env_var("PROJECT_ID", error=True)
-ISS_API_KEY=get_env_var("ISS_API_KEY", error=True)
+ISS_API_KEY = get_env_var("ISS_API_KEY")
 ISS_API_KEY_NAME=get_env_var("ISS_API_KEY_NAME", error=True)
-ISS_KEY_TABLE_NAME=get_env_var("ISS_KEY_TABLE_NAME", error=True)
+ISS_KEY_TABLE_NAME = get_env_var("ISS_KEY_TABLE_NAME")
 ISS_SCMS_TOKEN_REST_ENDPOINT=get_env_var("ISS_SCMS_TOKEN_REST_ENDPOINT", error=True)
 ISS_SCMS_VEHICLE_REST_ENDPOINT=get_env_var("ISS_SCMS_VEHICLE_REST_ENDPOINT", error=True)
 ISS_PROJECT_ID=get_env_var("ISS_PROJECT_ID", error=True)
